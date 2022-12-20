b1db9f2a238ca6632ea8c18dc59e964845c63eda
Aditya Sharma <adsh@odoo.com>
2022-12-20 18:51:48 +0530

    basics of model formation

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    "name":"Real Estate",
+    "version":"1.0",
+    "author":"Aditya Sharma",
+    "category":"Advertising",
+    "description": 'Real estate Advertising module',
+    "application": True,
+}
