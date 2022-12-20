3f9ffd8f38f31169c1b15a60e3e9a1b3fa925e37
Deependra Solanki <deso@odoo.com>
2022-12-20 18:51:40 +0530

    Basics of Models and Fields

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Real Estate',
+    'version' : '1.0',
+    'summary' : 'Manage Real Estate Properties',
+    'depends' : ['base'],
+    'installable' : True,
+    'application' : True,
+}
