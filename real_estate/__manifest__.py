e177158e167228e37479dfda83d1f6cae18e5992
Renilkumar Kajavadra <reka@odoo.com>
2022-12-20 18:51:56 +0530

    Create model of Real Estate

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Real Estate',
+    'description': 'Cozy Appartment in brussels surroundings',
+    'application' : True
+
+}
---
+++ real_estate/models/form.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields
+
+class realEstate(models.Model):
+    _name = "real_estate"
+    _description = "The Real Estate Advertisement Module"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(string='Type',
+        selection=[('lead', 'Lead'), ('opportunity', 'Opportunity')],
+        help="Type is used to separate Leads and Opportunities"
+        )
+
+
+
+
