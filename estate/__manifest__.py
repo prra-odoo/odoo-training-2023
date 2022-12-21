bf2b98e54b55fd3b1ff1e4203650a50b83422bb8
Saurabh Choraria <sauc@odoo.com>
2022-12-21 18:54:12 +0530

    created basic view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,5 +5,11 @@
     'version' : '1.0',
     'author' : "Saurabh Choraria",
     'description' : "It is a module for real estate",
+    'data' : [
+            # 'data/estate_property_demo.xml',
+            'views/estate_property_views.xml',
+            'security/ir.model.access.csv',
+        ],
+
     'application' : True,
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+import datetime
 
 class EstateModel(models.Model):
     _name = "estate.model"
     _description = "Estate Model"
 
-    name = fields.Char(required = True)
+    name = fields.Char(required = True, readonly = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_avilability = fields.Date()
-    expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
+    date_avilability = fields.Date('date availability', default = lambda self: self.datetime.now())
+    expected_price = fields.Float('expected price', required = True)
+    selling_price = fields.Float('selling price', copy = False)
     bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    living_area = fields.Integer('living area')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer('garden area')
     garden_orientation = fields.Selection(
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
