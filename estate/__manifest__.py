7af0cb23d91b17f4439b063bbded6f6d936953ea
maad-odoo <maad@odoo.com>
2022-12-21 19:01:33 +0530

    created a basic view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-
-
 {
     'name' : 'Real Estate',
     'description' : 'Real Estate Advertising Module',
     'application' : True,   
+    'data' : [
+        'security/ir.model.access.csv',
+        # 'data/estate_demodata.xml',
+        'views/estate-view.xml',
+    ],
 }
--- estate/models/estate_module.py
+++ estate/models/estate_module.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+from datetime import datetime, timedelta
 
 class estateModel(models.Model):
     _name = "estate.model"
     _description = "Real Estate Module"
 
-    name = fields.Char()
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char('Name',required=True,copy=False)
+    description = fields.Text('Description',required=True)
+    postcode = fields.Char(required=True)
+    date_availability = fields.Datetime("Last Availability",default=lambda self:fields.Datetime.now())
+    expected_price = fields.Float('Expected Price',readonly=True)
+    selling_price = fields.Float(required=True,copy=False)
+    bedrooms = fields.Integer(required=True)
+    living_area = fields.Integer(copy=False)
+    facades = fields.Integer(required=True)
+    garage = fields.Boolean(required=True)
+    garden = fields.Boolean(required=True)
+    garden_area = fields.Integer(required=True)
     garage_orientation = fields.Selection(
         string='Type',
         selection=[('east', 'East'), ('west', 'West'),('north','North'),('south','South')],
         help="Type is used to separate Leads and Opportunities")
+
+
+    
