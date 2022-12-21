3de0810a3651f3bea674f0ef03efe8e5dbcac6fa
Yashkumar Vaishnav <yava@odoo.com>
2022-12-21 18:51:00 +0530

    Create View for Real Estate Modual

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,8 +4,12 @@
     'name': "Real Estate",
     'version': '1.0',
     'author': "yava",
-    'category': 'Category',
     'description': "You can easily apply your core project modual with odoo's Real Estate ",
+    'data': [
+        'data/estate_menus.xml',
+        'views/estate_property_views.xml',
+        'security/ir.model.access.csv',
+    ],
     'application': True,
     'installable': True,
     'license' : "LGPL-3",
--- estate/models/form.py
+++ estate/models/form.py
@@ -7,18 +7,18 @@ class realEstate(models.Model):
     _name = "real_state"
     _description = "This is the Database for the all clients and their requirements"
 
-    name = fields.Char( required=True )
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float( required =True )
-    selling_price = fields.Float( required =True )
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char( "Name",required=True )
+    description = fields.Text("Description")
+    postcode = fields.Char("Post Code")
+    date_availability = fields.Date("Date Availability",default=lambda self: fields.Datetime.now())  
+    expected_price = fields.Float( "Excepted Price",required =True )
+    selling_price = fields.Float( "Selling Price",required =True, readonly=True)
+    bedrooms = fields.Integer("Bedrooms")
+    living_area = fields.Integer("Living Area",default="1   ")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("garage")
+    garden = fields.Boolean("garden",default=1)
+    garden_area = fields.Integer("Garden Area")
     garden_orientation = fields.Selection(
-        selection=[('3BHK', 'Size'), ('Home','Type')]
+        selection=[('small', 'Small'), ('big','Big')]
         )
