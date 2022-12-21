89e02ce0eb5056e1d7d6a60acd93ddbc9bd3f1d6
Deependra Solanki <deso@odoo.com>
2022-12-21 18:49:42 +0530

    views and demo data day 2

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -4,6 +4,11 @@
     'version' : '1.0',
     'summary' : 'Manage Real Estate Properties',
     'depends' : ['base'],
+    'data' : [
+        'data/estate_demo_data.xml',
+        'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+    ],
     'installable' : True,
     'application' : True,
 }
