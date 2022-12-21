26f83913ea5779106fdb634845bbc8afeab4b7d0
Sanket Brahmbhatt <sabr@odoo.com>
2022-12-21 18:52:17 +0530

    creating view,data etc. file

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -5,6 +5,12 @@
     'author' : "Sanket Brahmbhatt(sabr)",
     'summary' : "Creating Real Estate Model",
     'version' : '1.0' ,
+    'data' : [
+              'data/estate_demodata.xml',
+              'views/estate_property_views.xml',
+              'security/ir.model.access.csv',
+    ],
     'application' : True,
-    'installable': True
+    'installable': True,
+    'license': 'LGPL-3',
 }
--- real_estate/models/real_estate_advertisement.py
+++ real_estate/models/real_estate_advertisement.py
@@ -6,18 +6,18 @@ class RealEstateAd  (models.Model):
      _name = "real.estate"
      _description = "Real Estate Advertisement module" 
 
-     name = fields.Char() 
-     description = fields.Text()
-     postcode =fields.Char()
-     date_availability=fields.Date()
-     expected_price = fields.Float()
-     selling_price = fields.Float()
-     bedrooms =fields.Integer()
-     living_area=fields.Integer()
-     facades=fields.Integer()
-     garage=fields.Boolean()
-     garden=fields.Boolean()
-     garden_area =fields.Integer()
+     name = fields.Char(string='Name',required=True) 
+     description = fields.Text(string='Description')
+     postcode =fields.Char(string='Postcode',required=True)
+     date_availability=fields.Date(string='Date Available',required=True)
+     expected_price = fields.Float(string='Price',required=True)
+     selling_price = fields.Float(string='Selling Price',required=True)
+     bedrooms =fields.Integer(string='Bedrooms')
+     living_area=fields.Integer(string='Living Area')
+     facades=fields.Integer(string='Facades')
+     garage=fields.Boolean(string='Garage')
+     garden=fields.Boolean(string='Garden',required=True)
+     garden_area =fields.Integer(string='Garden Area')
      garden_orientation =fields.Selection(
           string='Type',
           selection=[('south','South'),('west', 'West'),('north','North'),('east', 'East')],
