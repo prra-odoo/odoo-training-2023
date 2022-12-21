66be194451a43f37f420e33e7dd31bc3959848d4
althaf shaik <alsh@odoo.com>
2022-12-21 18:58:53 +0530

    created csv file,demodata,views

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'real estate',
-    'description' : "",
-    'version' : '',
+    'description' : " Real estate module",
+    'version' : '1.0',
+    'author':'Althaf Shaik',
+    'category':'Advertising',
+    'data':[
+        'data/demo_data.xml',
+         'security/ir.model.access.csv',
+         'views/estate_property_views.xml',
+      
+        
+       
+    ],
     'application' : True,
-    'installable' : True
+    
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,18 +2,18 @@ from odoo import models,fields
 
 class estateProperty(models.Model):
     _name = "estate.property"
-    _order = "sequence"
+    
     _description = "This is the description for the estate property"
 
 
     name = fields.Char(required=True)
-    id = fields.Integer()
-    description = fields.Text()
+    id = fields.Integer(required=True)
+    description = fields.Text(copy=False)
     postcode = fields.Char()
     date_availability = fields.Date()
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(required=True,readonly=True)
+    bedrooms = fields.Integer(default=4)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
