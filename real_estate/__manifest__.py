40a4e0aba6f44def31b9458dd6a08745eae9cc62
Sanket Brahmbhatt <sabr@odoo.com>
2022-12-22 18:41:09 +0530

    add demo data and view files

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -6,11 +6,15 @@
     'summary' : "Creating Real Estate Model",
     'version' : '1.0' ,
     'data' : [
-              'data/estate_demodata.xml',
+            #   'data/estate_demodata.xml',
               'views/estate_property_views.xml',
+              'views/estate_property_actions.xml',
               'security/ir.model.access.csv',
     ],
     'application' : True,
     'installable': True,
     'license': 'LGPL-3',
+    'demo': [
+        'demo/estate_property_demo_data.xml',
+    ],
 }
--- real_estate/models/real_estate_advertisement.py
+++ real_estate/models/real_estate_advertisement.py
@@ -6,20 +6,19 @@ class RealEstateAd  (models.Model):
      _name = "real.estate"
      _description = "Real Estate Advertisement module" 
 
-     name = fields.Char(string='Name',required=True) 
+     name = fields.Char(string='Name') 
      description = fields.Text(string='Description')
-     postcode =fields.Char(string='Postcode',required=True)
-     date_availability=fields.Date(string='Date Available',required=True)
-     expected_price = fields.Float(string='Price',required=True)
-     selling_price = fields.Float(string='Selling Price',required=True)
+     postcode =fields.Char(string='Postcode')
+     date_availability=fields.Date(string='Date Available',default=lambda self: fields.Datetime.now())
+     expected_price = fields.Float(string='Price')
+     selling_price = fields.Float(string='Selling Price')
      bedrooms =fields.Integer(string='Bedrooms')
      living_area=fields.Integer(string='Living Area')
      facades=fields.Integer(string='Facades')
      garage=fields.Boolean(string='Garage')
-     garden=fields.Boolean(string='Garden',required=True)
+     garden=fields.Boolean(string='Garden')
      garden_area =fields.Integer(string='Garden Area')
      garden_orientation =fields.Selection(
           string='Type',
-          selection=[('south','South'),('west', 'West'),('north','North'),('east', 'East')],
-         
-     )
+          selection=[('south','South'),('west', 'West'),('north','North'),('east', 'East')]   
+          )
