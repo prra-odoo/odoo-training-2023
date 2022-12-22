1804aef4e19271eded478ecf039a9102a0c22571
Pratik Raval <prra@odoo.com>
2022-12-22 17:50:04 +0530

    basics of Fields, views, action-menu  and demo data

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -2,5 +2,12 @@
 {
 	'name' : 'Open Academy',
 	'description' : 'Open Academy Module',
+	'data' : [
+			'security/ir.model.access.csv',
+			'views/course_views.xml',
+			],
+	'demo' : [
+			'demo/academy_course_demo_data.xml',
+	],
 	'application' : True,
 }
--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -6,4 +6,6 @@ class openAcademyCourse(models.Model):
 	_name = 'academy.course'
 	_description = 'Open Academy Model for Course'
 
-	name = fields.Char() # Integer()
+	name = fields.Char('Name', required=True, readonly=True, copy=False)
+	description = fields.Text(default='basic description')
+	date = fields.Date('Date', default=lambda self: fields.Date.today())
