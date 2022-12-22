7cb322668dd5e06a1f3cb30368680787db6c9581
Vivek Sinha <vksi@odoo.com>
2022-12-22 18:40:28 +0530

    Today i created form view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,9 +4,12 @@
     'name' : 'Estate Property',
     'description' : 'Advertisment Module of Real Estate',
     'data' :[
-        'data/estate_demo_data.xml',
         'security/ir.model.access.csv',
+        'views/estate_views_actions.xml',
         'views/estate_views.xml'
     ],
+    'demo' :[
+        'demo/estate_demo_data.xml',
+        ],
     'application' : True
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,17 +8,17 @@ class test(models.Model):
     
     
     name = fields.Char(required = True)
-    description = fields.Text(copy =False)
     postcode = fields.Char()
-    date_availability = fields.Date("Last Availability",default=lambda self:fields.Datetime.now())
-    expected_price = fields.Float(required=True)
-    selling_price=fields.Float(readonly=True)
-    bedrooms = fields.Integer()
-    living_areas = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection(
+    description = fields.Text(required = True)
+    date_availability = fields.Date("Last Availability Date",default=lambda self:fields.Datetime.now())
+    expected_price = fields.Float(default = 550000)
+    selling_price=fields.Float(default = 500000)
+    bedrooms = fields.Integer(required = True, default = 50)
+    living_area = fields.Integer(default = 2)
+    facades = fields.Integer(default = 5)
+    garage = fields.Boolean(default= False)
+    garden_area = fields.Integer(default = 1)
+    garage_orientation = fields.Selection(
         string = 'Type',
         selection=[('North','East'),('South','West')],
         help = 'Type is used to separate the Directions'
