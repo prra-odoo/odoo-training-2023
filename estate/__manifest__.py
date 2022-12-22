d0f6f76e0533cc37679701dcf9dd73833d351ad5
Preksha Chouhan <prec@odoo.com>
2022-12-22 18:46:46 +0530

    2nd day

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,5 +2,12 @@
 {
     'name' : 'Real Estate',
     'description' : """"this is the real estate advertisement module""",
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/estate_property_view.xml',
+    ],
+    'demo': [
+        'demo/estate_demo_data.xml',
+    ],
     'application' : True,
 }
--- estate/model/estate_property.py
+++ estate/model/estate_property.py
@@ -1,23 +1,25 @@
+# -*- coding: utf-8 -*-
+
 from odoo import models, fields
 
 class TestModel(models.Model):
-    _name = "test.model"
-    _description = "Test model"
+    _name = "estate.property"
+    _description = "Real estate advertisement module"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name = fields.Char('Name',required=True)
+    description = fields.Text('Details',copy=False)
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Date availability',default=lambda self:fields.Datetime.now(),readonly=True)
+    expected_price = fields.Float('Expected price',required=True)
+    selling_price = fields.Float('Selling price',readonly=True)
+    bedrooms = fields.Integer('Bedroom',default='2')
+    living_area = fields.Integer('Living area')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden area')
     garden_orientation = fields.Selection(
-         string='Type',
-        selection=[('East', 'West'), ('North', 'South')],
+        string='Garden orientation',
+        selection=[('West', 'East'), ('North', 'South')],
         help="Type is used to separate the directions"
     )
