db35410fa724e8fe556c444d3710ec936c86b169
Pratik Raval <prra@odoo.com>
2022-12-23 15:04:25 +0530

    Reserved Fields, Widget intro. and view enhancements

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -6,6 +6,8 @@ class openAcademyCourse(models.Model):
 	_name = 'academy.course'
 	_description = 'Open Academy Model for Course'
 
-	name = fields.Char('Name', required=True, readonly=True, copy=False)
-	description = fields.Text(default='basic description')
-	date = fields.Date('Date', default=lambda self: fields.Date.today())
+	name = fields.Char('Name', required=True)
+	description = fields.Text(default='basic description', copy=False)
+	date = fields.Date('Date', default=lambda self: fields.Date.today(), readonly=True)
+	active = fields.Boolean(default=True)
+	state = fields.Selection([('new', 'New'), ('confirm', 'Confirm'), ('cancel', 'Cancel')], default="new")
