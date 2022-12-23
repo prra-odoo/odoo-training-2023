959596136730060670ed79788e50aec8529084f8
maad-odoo <maad@odoo.com>
2022-12-23 18:58:34 +0530

    enhanced the look of the form view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,8 +5,7 @@
     'application' : True,   
     'data' : [
         'security/ir.model.access.csv',
-        # 'data/estate_demodata.xml',
-        'views/estate-view.xml',
+        'views/estate_view.xml',
         'views/estate_view_action.xml',
     ],
     'demo' : [
--- estate/models/estate_module.py
+++ estate/models/estate_module.py
@@ -4,13 +4,13 @@ from odoo import models, fields
 from datetime import datetime, timedelta
 
 class estateModel(models.Model):
-    _name = "estate.model"
+    _name = "estate.property"
     _description = "Real Estate Module"
 
     name = fields.Char('Name',required=True)
     description = fields.Text('Description',copy=False,required=True)
     postcode = fields.Char('Post Code',required=True)
-    date_availability = fields.Datetime('Last Availability',default=lambda self:fields.Datetime.now())
+    date_availability = fields.Date('Last Availability',default=lambda self:fields.Datetime.today())
     expected_price = fields.Float('Expected Price',required=True)
     selling_price = fields.Float('Selling Price',copy=False,required=True)
     bedrooms = fields.Integer('Bedrooms',required=True)
@@ -19,6 +19,8 @@ class estateModel(models.Model):
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
+    active = fields.Boolean(default=True)
+    state=fields.Selection(selection=[('new', 'New'), ('inprogress', 'In Progress'),('done','Done')],default='new')
     garage_orientation = fields.Selection(
         string='Garden Orientation:',
         selection=[('east', 'East'), ('west', 'West'),('north','North'),('south','South')],
