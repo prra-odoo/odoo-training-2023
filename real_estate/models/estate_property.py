1964ec62e9ecfc66334754baa8bd7d92f768407a
Om Rabara <omra@odoo.com>
2022-12-23 18:48:47 +0530

    Header and form view

---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,25 @@
+# -- coding: utf-8 --
+
+from odoo import fields, models
+
+class estateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate model"
+
+    name = fields.Char('Name :', required = True)
+    description = fields.Text('Description ')
+    postcode = fields.Char('Postcode ')
+    date_availability = fields.Date(default=lambda self: fields.Datetime.now())
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer('Bedrooms ')
+    living_area = fields.Integer('Living Area ')
+    facades = fields.Integer('Facades ')
+    garage = fields.Boolean('Garage ')
+    garden = fields.Boolean('Garden ')
+    garden_area = fields.Integer('Garden Area ')
+    garden_orientation = fields.Selection(string="Orientation ",
+        selection=[('north', 'North'), ('south', 'South'), ('east','East'), ('west','West')],
+        help="Type is used to separate Leads and Opportunities")
+    state = fields.Selection(selection= [('new','New'),('confirm','Confirm'),('cancel','Cancel')], default="new")
+    activate = fields.Boolean(default=True)
