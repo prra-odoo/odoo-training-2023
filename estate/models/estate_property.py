06f8cfe05e6396722a0c6ccd016654f250176e30
Vivek Sinha <vksi@odoo.com>
2022-12-26 12:03:07 +0530

    I have made some changes and imoproved my code

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,8 +8,8 @@ class test(models.Model):
     
     
     name = fields.Char(required = True)
-    postcode = fields.Integer(default = 1001)
-    description = fields.Text("Estate Property",required = True)
+    postcode = fields.Integer(default = 104,readonly=True)
+    description = fields.Text("Estate Property",required = True,copy=False)
     date_availability = fields.Date("Last Availability Date",default=lambda self:fields.Datetime.now())
     expected_price = fields.Float(default = 550000)
     selling_price=fields.Float(default = 500000)
@@ -19,6 +19,7 @@ class test(models.Model):
     garage = fields.Boolean(default= False)
     garden_area = fields.Integer(default = 1)
     Other_info = fields.Text("Others info",required=True)
+    log_note=fields.Text("Log Note")
     garage_orientation = fields.Selection(
         string = 'Garage_Orientation',
         selection=[('north','North'),('south','South'),('east','East'),('west','West')]
