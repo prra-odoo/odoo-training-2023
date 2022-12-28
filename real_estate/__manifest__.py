cf86e6aee014658a8f1f31fd0142b5202e96e14e
Dhrutik Patel <dhrp@odoo.com>
2022-12-28 18:47:33 +0530

    Manifest file modified.

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Dhrutik Patel (dhrp)',
+    'category': 'Sales',
+    'website': 'https://www.odoo.com/',
+    'description': 'Real Estate Module',
+    'summary': 'Real Estate Module',
+    'installable': True,
+    'application': True,
+}   
