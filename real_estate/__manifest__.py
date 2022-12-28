40aff1b807b0aa524e771c61dba443f0d83924e7
Saurabh Mishra <sami@odoo.com>
2022-12-28 19:07:12 +0530

    Created Manifest in estate

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'real_estate',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Find rates of real estates',
+    'description': "",
+    'author': 'sami',
+    'installable': True,
+    'application': True,
+    'auto_install': False
+
+}
