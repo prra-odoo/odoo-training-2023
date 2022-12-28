bd8e555ad9e4d04862f29d31c0b4af9f95ac8a67
Harsh Modi <hamo@odoo.com>
2022-12-28 18:56:42 +0530

    Second Commit

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Real Estate',
+    'version':'0.1',
+    'depends':['base'],
+    'author':'Harsh Modi (hamo)',
+    'category':'Sales',
+    'data':[
+
+    ],
+    'demo':[
+        
+    ],
+    'summary':'A real estate module',
+   'installable':'true',
+   'application':'true',
+}
