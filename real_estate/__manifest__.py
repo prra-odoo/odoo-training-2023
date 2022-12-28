2680bb697d485952c0a412808a8e138e74a8c5bf
Archana Vaghasiya <arva@odoo.com>
2022-12-28 18:33:57 +0530

    empty module create

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate',
+    'version': '1.2',
+    'category': 'Sales/CRM',
+    'depends':['contacts','calender'],
+    'author': 'Archana',
+    'summary': 'Track leads and close opportunities',
+    'description': 'Real estate module',
+    'installable': True,
+    'application': True,
+}
