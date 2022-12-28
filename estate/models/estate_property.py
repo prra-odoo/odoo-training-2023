61c1f443691d69d70a9a59325b6e77db5a7a2c52
Aditya Sharma <adsh@odoo.com>
2022-12-28 18:21:24 +0530

    Understanding the Many2one

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -31,5 +31,5 @@ class estatePropertyModel(models.Model):
         default='new',
     )
     property_type_id= fields.Many2one("estate.property.type", string="Type")
-    salesperson = fields.Many2one("res.partner",string="Salesperson", default=lambda self: self.env.user)
-    buyer= fields.Many2one("res.users", string="Buyers" ,copy= False)
+    salesperson_id = fields.Many2one("res.users",string="Salesperson", default=lambda self: self.env.user)
+    buyer_id= fields.Many2one("res.partner", string="Buyers" ,copy= False)
