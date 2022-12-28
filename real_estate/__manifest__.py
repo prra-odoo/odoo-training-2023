96ff6924b6c98412331cbc7586169cf1b27aeb06
Rahul Reddiar <rare@odoo.com>
2022-12-28 18:57:34 +0530

    Manifest-Updated

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "rare",
+    'category': 'sales',
+    'description': """
+        This is my Real Estate Module 
+    """,
+    # 'installable': True,
+    # 'application': True,
+    # data files always loaded at installation
+}
