8727c960c568a358d25e2022d65dfe928141cc1b
Maitri Patel <Maitri Patel>
2022-12-28 18:10:46 +0530

    28_commit

---
+++ real-estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'REAL_ESTATE',
+    'version': '2.0',
+    'depends': ['base'],
+    'author': 'odoo',
+    'category': 'main',
+    'description': 'Real Estate Module',
+    'summary': 'real estate advertisement',
+    'installable': True,
+    'application': True,
+    'auto_install': False    
+}
