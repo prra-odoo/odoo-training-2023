bc20dec67782c48c3eabcff3055686263ab73bd7
thsh-odoo <thsh@odoo.com>
2022-12-28 18:44:28 +0530

    Dec_28_Commit

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'RealEstate',
+    'category' : 'Sales',
+    'summary' : 'Selling Your property',
+    'description' : 'This app will help you to find the best deal for your properties',
+    'version' : '1.0',
+    'author' : 'THSH',
+    'installable' : True,
+    'application' : True,
+    'auto_install' : False
+}
