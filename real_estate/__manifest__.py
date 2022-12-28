ea14e54fa51cb1162a333caa7fa98cc19b0a59e3
Gurupreet Singh <gusi@odoo.com>
2022-12-28 18:56:31 +0530

    Init File and Menifest File First Commit

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Real Estate",
+    'version':'1.0',
+    'depends': ['base'],
+    'author': "Gurpreet Singh(gusi)",
+    'category':'Sales',
+    'description': "This is a Real Esate App",
+    'summary': "This is a Real Esate App developed in odoo(OWL Framework)",
+    'installable': True,
+    'application': True,
+}
