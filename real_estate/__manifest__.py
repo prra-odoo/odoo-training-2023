25486be0b5f8933e40b67e2e22b22dd4dd0e7168
thsh-odoo <thsh@odoo.com>
2022-12-29 14:22:47 +0530

    pycache and some basic modifications in manifest file

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -5,6 +5,9 @@
     'description' : 'This app will help you to find the best deal for your properties',
     'version' : '1.0',
     'author' : 'THSH',
+    'data' : [    ],
+    'demo' : [     ],
+    'depends' : [     ],
     'installable' : True,
     'application' : True,
     'auto_install' : False
