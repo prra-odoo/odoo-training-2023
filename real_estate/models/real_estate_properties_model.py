f257bf394d996d028b88b760411d31146980d5b8
thsh-odoo <thsh@odoo.com>
2022-12-29 19:01:55 +0530

    Created the models and created fields

---
+++ real_estate/models/real_estate_properties_model.py
@@ -0,0 +1,21 @@
+from odoo import fields,models
+
+class real_Esate_Properties(models.Model):
+
+	_name='real.state.properties'
+	_description="Selling your properties"
+
+
+	name=fields.Char(required=True)
+	description=fields.Text()
+	Postcode=fields.Char()
+	date_availablity=fields.Date()
+	expected_price=fields.Float(required=True)
+	selling_price=fields.Float()
+	bedroom=fields.Integer()
+	living_area=fields.Integer()
+	facades=fields.Integer()
+	garage=fields.Boolean()
+	gardan=fields.Boolean()
+	gardan_area=fields.Integer()
+	
