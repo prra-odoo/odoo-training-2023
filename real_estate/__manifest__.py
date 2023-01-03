c69d1f40e08a3d6c2227dbdb3614f0f10ba7aa21
Gurupreet Singh <gusi@odoo.com>
2023-01-03 17:47:34 +0530

    Adding list(tree) view and form view

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -8,6 +8,6 @@
     'summary': "This is a Real Esate App developed in odoo(OWL Framework)",
     'installable': True,
     'application': True,
-    'data':['./security/ir.model.access.csv'],
+    'data':['security/ir.model.access.csv','views/estate_property_views.xml','views/real_estate_menu.xml',],
     'demo' :[],
 }
