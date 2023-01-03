98ac5a47d12ae5fbd8cf0817e95fbfd8abf03ee1
Om Rabara <omra@odoo.com>
2023-01-03 18:59:43 +0530

    compute,inverse and some actions

--- real_estate/models/estate_offer.py
+++ real_estate/models/estate_offer.py
@@ -1,4 +1,6 @@
 from odoo import fields, models 
+from odoo import api
+from odoo.tools.date_utils import add
 
 class propertyoffer(models.Model):
     _name="estate.property.offer"
@@ -8,3 +10,24 @@ class propertyoffer(models.Model):
     partner_id = fields.Many2one("res.partner" ,required=True)
     property_id = fields.Many2one("estate.property",required=True)
     status = fields.Selection(selection= [('accepted','Accepted'),('refused','Refused')])
+    validity = fields.Integer()
+    date_deadline = fields.Date("Date Deadline", compute='_compute_date_deadline',inverse='_inverse_date_deadline')
+    create_date = fields.Date(default=fields.Datetime.now(),readonly =True)
+
+    @api.depends('validity','create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = add(record.create_date,days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline:
+                record.validity = (record.date_deadline - record.create_date).days
+
+    def accept_action(self):
+        for record in self:
+            record.status='accepted'
+    
+    def refused_action(self):
+        for record in self:
+            record.status='refused'
--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -1,6 +1,8 @@
 # -- coding: utf-8 --
 
 from odoo import fields, models
+from odoo import api
+from odoo.exceptions import UserError
 
 class estateProperty(models.Model):
     _name = "estate.property"
@@ -21,10 +23,41 @@ class estateProperty(models.Model):
     garden_orientation = fields.Selection(string="Orientation ",
         selection=[('north', 'North'), ('south', 'South'), ('east','East'), ('west','West')],
         help="Type is used to separate Leads and Opportunities")
-    state = fields.Selection(selection= [('new','New'),('confirm','Confirm'),('cancel','Cancel')], default="new")
+    state = fields.Selection(selection= [('new','New'),('confirm','Confirm'),('cancel','Cancel'),('sold_action','Sold'),('cancel_action','Cancel')], default="new")
     activate = fields.Boolean(default=True)
     property_type_id = fields.Many2one("estate.property.type",string= "Property type")
     salesman_id=fields.Many2one("res.users",string="salesman",default=lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", string="Buyers")
     tag_ids = fields.Many2many("estate.property.tag", string= "Property tag")
     offer_ids = fields.One2many("estate.property.offer","property_id")
+    total_area= fields.Float(compute='_compute_total_area')
+    best_price = fields.Float(compute='_compute_best_price')
+    status =fields.Char()
+
+    @api.depends('living_area','garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+        
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'),default=0)
+
+    def sold_action(self):
+        for record in self:
+            if record.state=='cancel':
+                raise UserError(('Cancel Property can not be sold'))
+            else:
+                record.state='sold_action'
+
+    def cancel_action(self):
+        for record in self:
+            if record.state=='sold':
+                raise UserError(("Sold property can't be canceled"))
+            else:
+                record.state='cancel_action'
+            
+    # def accept_action(self):
+    #     for record in self:
+    #         record.
