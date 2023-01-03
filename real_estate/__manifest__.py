09878a2cc5d9725a04fd33e021dd2d1bb0cd9b55
Tejas Modi <temo@odoo.com>
2023-01-03 17:36:13 +0530

    Views and action

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -9,10 +9,12 @@
     'installable':'true',
     'application':'true',
     'data': [
-        'security/ir.model.access.csv'
-
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_action.xml',
+        
     ],
-    'demo':[
+    # 'demo':[
 
-    ],
+    # ],
 }
--- real_estate/models/estate.py
+++ real_estate/models/estate.py
@@ -1,4 +1,6 @@
 from odoo import models,fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class Estate(models.Model):
     _name="real.estate.properties"
@@ -7,12 +9,14 @@ class Estate(models.Model):
     name=fields.Char(string="Title",required=True)
     description=fields.Text('Description')
     postcode=fields.Char('Postcode')
-    date_availability=fields.Date('Date Availabilty')
+    date_availability=fields.Date('Date Availabilty',copy=False,default=date.today()+relativedelta(months=3))
     expected_price=fields.Float('Expected Price',required=True)
-    selling_price=fields.Float('Selling Price',required=True)
-    bedrooms=fields.Integer('Bedrooms')
+    selling_price=fields.Float('Selling Price',required=True,copy=False,readonly=True)
+    bedrooms=fields.Integer('Bedrooms',default=2)
     living_area=fields.Integer('Living Area')
     facades=fields.Integer('Facades')
     garage=fields.Boolean('Garage')
     garden=fields.Boolean('Garden')
     garden_orientation=fields.Selection(selection=[('north','North'), ('south','South'), ('east','East'),('west','West')])
+    active=fields.Boolean(default=True)
+    state=fields.Selection(selection=[('new','New'),('offer recevied','Offer Received'),('offer accepted','Offer Accepted'),('sold','Sold'),('canceled','Canceled')],default='new' ,required=True,copy=False)
