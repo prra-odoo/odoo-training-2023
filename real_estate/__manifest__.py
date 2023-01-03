60f48102f5a12c6bdd6996917f0625e7f793d137
Mohit Beniwal <mobe@odoo.com>
2023-01-03 17:46:55 +0530

    created views and menus and added more fields in model

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Real Estate',
     'version': '1.0',
@@ -5,7 +7,9 @@
     'author': 'mobe',
     'category' :'Marketing',
     'data' : [
-         'security/ir.model.access.csv'
+         'security/ir.model.access.csv',
+         'views/estate_property_views.xml',
+         'views/estate_property_menus.xml',
 
     ],
     'demo' : [
--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -9,16 +9,17 @@ class estate_Property(models.Model):
     name=fields.Char('Property name',required=True)
     description=fields.Text('Property description')
     postcode=fields.Char('Postcode',required=True)
-    date_availability=fields.Date('Date availability')
+    date_availability=fields.Date('Date availability', copy=False)
     expected_price=fields.Float('expected price',required=True)
-    selling_price=fields.Float('selling price',required=True)
-    bedrooms=fields.Integer('bedrooms')
+    selling_price=fields.Float('selling price',required=True, readonly=True, copy=False)
+    bedrooms=fields.Integer('bedrooms', default=2)
     living_area=fields.Integer('living area')
     facades=fields.Integer('facades')
     garage=fields.Boolean('garage')
     garden=fields.Boolean('garden')
     garden_area=fields.Integer('garden area')
     garden_orientation = fields.Selection(string="Garden Orientation", selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-    
+    active= fields.Boolean('active')
+    state=fields.Selection(string="State",selection=[('new','New'),('offer_received','Offer received'),('offer_accepted','Offerf accepted'),('sold','Sold',),('canceled','Canceled')],default='new',required=True)
 
 
