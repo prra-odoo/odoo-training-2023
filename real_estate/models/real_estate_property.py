eac6255331e4bba29a00144df975435c1ca785fc
Harsh Modi <hamo@odoo.com>
2023-01-03 17:34:37 +0530

    Created Tree and Form view

--- real_estate/models/real_estate_property.py
+++ real_estate/models/real_estate_property.py
@@ -1,12 +1,12 @@
 from odoo import models,fields
-
+from dateutil.relativedelta import relativedelta
 class property(models.Model):
-    _name='real_estate.property'
+    _name='real.estate.property'
     _description="Property model"
     name=fields.Char(string='Property Name',required=True)
     description=fields.Text()
     postcode=fields.Char()
-    date_availability=fields.Date()
+    date_availability=fields.Date(default=fields.Datetime.now()+relativedelta(months=3))
     expected_price=fields.Float(required=True)
     selling_price=fields.Float()
     bedrooms=fields.Integer()
@@ -16,3 +16,5 @@ class property(models.Model):
     garden=fields.Boolean()
     garden_area=fields.Integer()
     garden_orientation=fields.Selection(selection=[('north','North'),('south','South'),('east','East'),('west','West')])
+    active=fields.Boolean(default=True)
+    state=fields.Selection(selection=[('new','New'), ('offer_received','Offer Received'), ('offer_accepted','Offer Accepted'), ('sold','Sold'),('canceled','Canceled')],default='new')
