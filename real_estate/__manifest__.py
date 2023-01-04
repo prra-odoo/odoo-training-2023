2179f70fd16af4a09379f2a73be9581ebf6bcbc0
Archana Vaghasiya <arva@odoo.com>
2023-01-04 18:22:23 +0530

    create basic search view

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -16,5 +16,6 @@
         'views/estate_property_views.xml',
         'views/estate_property_tree_view.xml',
         'views/estate_property_form_view.xml',
+        'views/estate_property_search_view.xml',
     ]
 }
