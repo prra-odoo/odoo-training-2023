4ac6dcab5fe9c2c900eaf37a320cff3bfc3c0648
thsh-odoo <thsh@odoo.com>
2023-01-10 17:15:14 +0530

    added computational and inverse field in module

--- real_estate/models/estate_property_offer.py
+++ real_estate/models/estate_property_offer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields
-
+from odoo import models,fields,api
+from dateutil.relativedelta import relativedelta
 class estate_property_offer(models.Model):
     _name="estate.property.offer"
     _description="Offers for a property"
@@ -14,4 +14,18 @@ class estate_property_offer(models.Model):
     )
     partner_id=fields.Many2one("res.partner",required=True)
     property_id=fields.Many2one("real.estate.properties",required=True)
+    validity=fields.Integer(default=7)
+    date_deadline=fields.Date(compute="_compute_date_deadline",inverse="_inverse_date_deadline")
     
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline=record.create_date + relativedelta(days=record.validity)
+            else:
+                record.date_deadline=fields.Datetime.now() + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            diff = record.date_deadline-record.create_date.date()
+            record.validity=int(diff.days)
--- real_estate/models/real_estate_properties_model.py
+++ real_estate/models/real_estate_properties_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields,models
+from odoo import api,fields,models
 from datetime import date
 from dateutil.relativedelta import relativedelta
 class real_Esate_Properties(models.Model):
@@ -37,8 +37,19 @@ class real_Esate_Properties(models.Model):
 	salesperson=fields.Many2one("res.users",default=lambda self: self.env.user)
 	tag_ids=fields.Many2many("estate.property.tag")
 	offer_ids=fields.One2many("estate.property.offer","property_id")
-	# total_area=fie
-		
-	
+	total_area=fields.Float(compute="_total_area")
+	best_price=fields.Float(compute="_best_price",default=0)
 
+	@api.depends("living_area","gardan_area")
+	def _total_area(self):
+		for area in self:
+			area.total_area=area.living_area+area.gardan_area
 
+	@api.depends("offer_ids.price")
+	def _best_price(self):
+		for value in self:
+
+			value.best_price=max(value.offer_ids.mapped('price'),default=0)
+			
+			# temp=(l.price for l in value.offer_ids)   without mapped
+			# value.best_price=max(temp,default=0)
