95b2b2439be9b34d61ee60d9f52ec6716d624af9
Maitri Patel <Maitri Patel>
2023-01-10 18:51:27 +0530

    compute and inverse field

--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -2,6 +2,8 @@
 from odoo import models,fields
 from dateutil.relativedelta import relativedelta
 
+from odoo import api
+
 
 class estate_Property(models.Model):
       _name = "estate.property"
@@ -30,14 +32,30 @@ class estate_Property(models.Model):
         selection=[('New','new'),('Offer Received','offer received'),('Offer Accepted','offer accepted'),
         ('Sold','sold'),('Canceled','canceled')],
       )
-      sales_id = fields.Many2one('estate.property.sales', string='Salesperson')
-      buyer_id=fields.Many2one('estate.property.buyer', string='buyer')
+      sales_id = fields.Many2one('res.partner', string='Salesperson')
+      buyer_id=fields.Many2one('res.users', string='buyer')
 
       type_id=fields.Many2one('estate.property.type',string="product type")
       tag_ids=fields.Many2many(
        'estate.property.tag', string='Tags',
       help="Classify and analyze your lead/opportunity categories like: Training, Service")
       offer_ids= fields.One2many('estate.property.offer','property_id')
+      total_area=fields.Float(compute='_compute_total_area')
+
+      @api.depends('living_area', 'garden_area')
+
+      def _compute_total_area(self):
+
+        for rec in self:
+
+          rec.total_area = rec.living_area+rec.garden_area
+      best_price=fields.Float(compute='_compute_best_price')
+      @api.depends('offer_ids.price')
+      def _compute_best_price(self):
+          for record in self:
+            record.best_price = max(self.offer_ids.mapped('price'),default=0)
+       
+        
 
 
 
--- real_estate/models/estate_property_offer.py
+++ real_estate/models/estate_property_offer.py
@@ -1,12 +1,29 @@
-from odoo import models,fields
+from odoo import models,fields,api
+from dateutil.relativedelta import relativedelta
+
 
 class estate_Property_offer(models.Model):
       _name = "estate.property.offer"
       _description = "Real estate based advertisedment module for the property type"
 
       price = fields.Float()
-      status = fields.Selection(string='status',selection=[('Accepted','accepted'),('Refused','refused'),copy=False])
+      status = fields.Selection(string='status',selection=[('Accepted','accepted'),('Refused','refused')],copy=False)
       partner_id = fields.Many2one("res.partner", string="Partner", required=True)
       property_id = fields.Many2one('estate.property',string="property id")
+      validity = fields.Integer(default='7')
+      date_deadline = fields.Date(compute="_compute_date_deadline",inverse="_inverse_date_deadline")
+      @api.depends('validity','date_deadline','create_date')
+      def _compute_date_deadline(self):
+            for record in self:
+                  if record.create_date:
+                   record.date_deadline = fields.Datetime.now() + relativedelta(days=record.validity)
+                  else:
+                   record.date_deadline = fields.Datetime.now() + relativedelta(days=record.validity)
+      
+      def _inverse_date_deadline(self):
+             for record in self:
+                  record.date_deadline = fields.Datetime.now() - relativedelta(days=record.validity).days
+                   
+      
 
-
+fields.Date.today() + relativedelta(months=+3)
