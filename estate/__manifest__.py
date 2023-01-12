f6d3f5b7c1aa9bda7450dbe64e7156c608ad5653
Aamir Achhava <aaac@gmail.com>
2023-01-12 18:46:25 +0530

    jan12

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,5 +3,12 @@
     'version': '1',
     'sequence': 1,
     'summary': 'Real estate around',
-    'description': "--demo description--"
+    'description': "--demo description--",
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+    ],
+    'installable': True,
+    'application': True,
 }    
