426a519a19d3379a12fe215c495e0a60ae3c36d0
RajPatani <rapa@odoo.com>
2023-01-12 16:05:14 +0530

    First Commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'category': "Category",
+    'summary': 'Manage Property Related Offers and more',
+    'description': "Real Estate module",
+    # 'data': [
+    #     'security/ir.model.access.csv'
+    # ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
+}
