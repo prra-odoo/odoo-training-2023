8916477087009ce641a178f132b45fc7c4d8a6d0
Aamir Achhava <aaac@gmail.com>
2023-01-13 11:29:23 +0530

    ..

--- estate/models/estateProperty.py
+++ estate/models/estateProperty.py
@@ -4,11 +4,19 @@ class estateProperty(models.Model):
     _name = "estate.property"
     _description="model description"
 
-    id=fields.Integer()
-    create_uid=fields.Integer()
-    create_date=fields.Date()
-    write_uid=fields.Integer()
-    write_date=fields.Date()
-    name=fields.Char()
-    description=fields.Char()
+    name=fields.Char(string="Name",required=True,help="this is name")
+    description=fields.Text(string="Description")
+    postcode=fields.Char(string="Pin Code",required=True)
+    date_availability=fields.Date(string="Availablity Date")
+    expected_price=fields.Float(string="Expected Price",required=True)
+    selling_price=fields.Float(string="Selling Price")
+    bedrooms=fields.Integer(string="Bedroom Number")
+    living_area=fields.Integer(string="Living Area")
+    facades=fields.Integer(string="Facades")
+    garage=fields.Boolean(string="Garage")
+    garden=fields.Boolean(string="Garden")
+    garden_area=fields.Integer(string="Garden Area")
+    garden_orientation=fields.Selection(
+        string="Garden Orientation",
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     
