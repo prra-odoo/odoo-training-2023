90c56d0a810eb52a52ed8160bb3a8710c00a2e50
Ayushi Gorai <aygo@odoo.com>
2023-01-13 17:20:48 +0530

    jan13 commit chp 6 and tasks

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,10 +6,11 @@
     'description': "real estate module",
     'installable':True,
     'application':True,
-    ''
 
      'data': [
         'security/ir.model.access.csv',
+        'view/estate_property_views.xml',
+        'view/estate_menus.xml',
      ]
     
 }
