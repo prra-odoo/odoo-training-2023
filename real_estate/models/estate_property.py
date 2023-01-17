83f8d1d72aec770455071dcdb106d815c86d0316
Renilkumar Kajavadra <reka@odoo.com>
2023-01-17 18:55:05 +0530

    model inheritance

--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -15,9 +15,9 @@ class estateProperty(models.Model):
     description = fields.Text(string='Description')
     postcode = fields.Integer(string='Postcode')
     date_availability = fields.Date(string='Available From', default=lambda self: fields.Datetime.now())
-    expected_price = fields.Float(string='Expected Price')
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price', copy=False, readonly=True)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
     living_area = fields.Integer(string='Living Area(sqm)')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
@@ -32,7 +32,7 @@ class estateProperty(models.Model):
     active = fields.Boolean(default = True)
     property_type_id = fields.Many2one("estate.property.type", string='Property Type')
     buyer_id = fields.Many2one("res.partner",string="Buyer",copy=False)
-    salesman_id = fields.Many2one("res.users",string="Salesperson", default=lambda self: self.env.user )
+    salesperson_id = fields.Many2one("res.users",string="Salesperson", default=lambda self: self.env.user )
     tag_ids=fields.Many2many("estate.property.tag", string='Tags')
     offer_ids=fields.One2many("estate.property.offer","property_id", string="Offer")
     total_area=fields.Float(string="Total Area", compute="_compute_total")
@@ -56,7 +56,7 @@ class estateProperty(models.Model):
     def sold_button(self):
         for record in self:
             if record.status=='cancel':
-                raise UserError(('Cancelled property can not be sold.'))
+                raise UserError(('Canceled property can not be sold.'))
             else:
                 record.status = 'sold'
                 record.state  = 'sold'
@@ -82,7 +82,7 @@ class estateProperty(models.Model):
                 if record.state not in ['new', 'cancel']:
                     raise UserError("Only New and Canceled Properties can be deleted.")
 
-    @api.model
-    def create(self, vals):
+    # @api.model
+    # def create(self, vals):
 
-        return super().create(vals)
+    #     return super().create(vals)
