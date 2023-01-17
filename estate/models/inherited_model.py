9e350ccb888bf5c5f1585e4a9f9834d42ee906a8
Yashkumar Vaishnav <yava@odoo.com>
2023-01-17 18:52:56 +0530

    Done work in Model inheritance

--- estate/models/inherited_model.py
+++ estate/models/inherited_model.py
@@ -8,3 +8,14 @@ class InheritedClass(models.Model):
 
     property_ids = fields.One2many('estate.property','salesperson_id',string="Property Id")
     demo = fields.Char('Demo')
+
+# class DelegationClass(models.Model):
+#     _name = "estate.property.customer"
+#     _inherits = {
+#         'estate.property' : 'property_type_id',
+#         'estate.property.offer' :'partner_id' 
+#     }
+
+#     property_type = fields.Many2one('estate.property',string="Estate Property",required=True,ondelete="cascade")
+#     partner = fields.Many2one('estate.property.offer',string="Estate Property",required=True,ondelete="cascade")
+
--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -17,16 +17,22 @@ class EstateAccount(models.Model):
                 "partner_id": rec.buyer_id.id,
                 "move_type": "out_invoice",
                 # "invoice_date": rec.date_availability,
-                "invoice_line_ids": [(
-                    0, 0,
-                    {
+                # "invoice_line_ids": [(
+                #     0, 0,
+                #     {
+                #         "name": rec.name,
+                #         'price_unit': rec.selling_price + 100.0,
+                #         'quantity': 1,
+                #         # 'tag_ids':Command.create({
+                #         # })F
+                #     }
+                # )],
+                'line_ids': [
+                    Command.create({
                         "name": rec.name,
-                        'price_unit': rec.selling_price + 100.0,
+                        'price_unit': rec.selling_price,
                         'quantity': 1,
-                        'tag_ids':Command.create({
-                            ''
-                        })
-                    }
-                )],
+                    })
+                ]
             })
         return super(EstateAccount, self).action_sold_button_header()
