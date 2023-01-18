1d7d394fb35218f47f80f73dafcd779b1b6ce837
maad-odoo <maad@odoo.com>
2023-01-18 19:19:14 +0530

    created a new inherited view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,11 +6,12 @@
     'depends': ['base', 'mail'], 
     'data' : [
         'security/ir.model.access.csv',
-        'views/estate_view.xml',
         'views/estate_view_action.xml',
+        'views/estate_view_menu.xml',
         'views/estate_offer.xml',
         'views/estate_type.xml',
         'views/estate_tags.xml',
+        'views/inherited_model.xml',
     ],
     'demo' : [
         'demo/estate_demodata.xml',
--- estate/models/inherited_model.py
+++ estate/models/inherited_model.py
@@ -6,4 +6,7 @@ class inheritedModel(models.Model):
     _inherit="res.users"
 
     property_ids = fields.One2many("estate.property","sales_id")
+    name=fields.Char()
+    nameusers=fields.Many2one("estate.property","name")
+    abc=fields.Char()
 
