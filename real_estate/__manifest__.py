3679d68833b16327b79e121516cf9fb53770a483
Tejas Modi <temo@odoo.com>
2023-01-19 18:48:14 +0530

    View Inheritance

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -15,7 +15,7 @@
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
-        # 'views/res_users_ad_views.xml',
+        'views/res_users_ad_views.xml',
     ],
      'demo':[
          'demo/estate_demo.xml',
--- real_estate/models/res_users_ad.py
+++ real_estate/models/res_users_ad.py
@@ -3,4 +3,4 @@ from odoo import api, models, fields
 class InheritedRes(models.Model):
     _inherit="res.users"
 
-    property_id=fields.One2many('real.estate.properties','user_id',domain="[('state', 'in', ('new','offer received))]")
+    property_ids=fields.One2many('real.estate.properties','user_id',string="Property",domain=[('state', 'in', ('new','offer received'))])
