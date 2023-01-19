34342806de817e30568fa94e51d1e6c9a9880c4c
Mohit Beniwal <mobe@odoo.com>
2023-01-19 19:04:26 +0530

    enhanced view inheritance and worked on self module

--- real_estate/models/res_users.py
+++ real_estate/models/res_users.py
@@ -5,4 +5,4 @@ from odoo import api,models,fields
 class InheritedUsers(models.Model):
     _inherit='res.users'
 
-    property_ids=fields.One2many('estate.properties', 'salesperson_id', string="inherited users", domain="['|', ('state', '=', 'new'), ('state', '=', 'offer_received')]")
+    property_ids=fields.One2many('estate.properties', 'salesperson_id', string="inherited users", domain=['|', ('state', '=', 'new'), ('state', '=', 'offer_received')])
