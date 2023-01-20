a2255041ac43c4d063adb441c70a5caad3a89fb5
Ayushi Gorai <aygo@odoo.com>
2023-01-20 19:04:59 +0530

    chap 9 compute date

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -10,12 +10,21 @@ class Estate_Property_Offer(models.Model):
     partner_id = fields.Many2one('res.partner',string = 'Parter',required=True)
     property_id = fields.Many2one('estate.property',string = 'Property',required=True)
 
-    validity = fields.Integer(string="Validity(days)")
-    date_deadline = fields.Date(string="Deadline")
-    create_date = fields.Date("")
+    #  validity = fields.Integer(string="Validity(days)", default=7)
+    #  date_deadline = fields.Date(string="Deadline", "_compute_dateline","_inverse_days")
+    #  create_date = fields.Date(string="")
+    
 
-    #validity date
+    # #validity date
 
-    @api.depends("validity","create_date")
-    def _compute_date_deadline(self):
-        for record in self:
+    # @api.depends("validity")
+    # def _compute_dateline(self):
+    #     for record in self:
+    #         record.date_deadline = record.date.today() + record.validity
+
+
+
+
+    # def _inverse_days(self):
+    #     for record in self:
+    #         record.validity = record.date_deadline-record.create_date
