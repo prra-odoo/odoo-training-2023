9f4a80e0f544c445b13223ae17b8060ceb693b63
Harsh Modi <hamo@odoo.com>
2023-01-23 18:40:23 +0530

    [ADD] estate_account: inherited real_estate_property and invoice generation on getting sold

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':'Estate Account',
+    'version':'0.1',
+    'depends':['real_estate','account'],
+    'category':'sales',
+    'demo':[],
+    'data':[],
+    'installable':'True',
+    'summary':'Real Estate Invoice Creation'
+}
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,26 @@
+from odoo import models,Command
+
+class EstateProperty(models.Model):
+    _inherit="real.estate.property"
+
+    def action_state_sold(self):
+        for record in self:
+            
+            self.env["account.move"].create({
+                'move_type':'out_invoice',
+                'partner_id': record.buyer_id.id,
+                
+                'invoice_line_ids':[
+                    Command.create({
+                        'name':record.name,
+                        'quantity':0.06,
+                        'price_unit':record.selling_price
+                    }),
+                    Command.create( {
+                        'name':'Administrative fees',
+                        'price_unit':100
+                    })
+                ],
+            })
+        print("\nhello world\n")
+        return super().action_state_sold()
