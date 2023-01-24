a9090729fa26aa68c285c5a0f3fd189ad3df092f
thsh-odoo <thsh@odoo.com>
2023-01-24 19:03:10 +0530

    [ADD] estate_project:  Created Task on project using a button
    Created a Module Estate_project and created a python file
    estate_property_project and inherit our main module to override the sold button
    to added the logic of the creation of task on sold button
    
    Completed the invoicing task in estate_account

---
+++ estate_project/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Estate Project',
+    'category' : 'sales',
+    'description' : 'Creating project and task after selling the property',
+    'version' : '1.0',
+    'author' : 'THSH',
+    'demo' : [
+
+    ],
+    'data' : [
+
+    ],
+    'depends' : ['real_estate','project'],
+    'installable' : True,
+    'auto_install' : False,
+    'application' : True
+}
