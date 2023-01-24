25e8ff1b465d9fb5bc501e1ab8078b33aab4f42d
Mohit Beniwal <mobe@odoo.com>
2023-01-24 18:49:25 +0530

    [ADD] real_estate,estate_account,estate_project:  Performed exercise of ch-14
    PURPOSE
    Firstly generated and empty invoice by selling the property and then added two
    lines while selling the property. Also created new module estate_project and
    linked project module with real estate module for creating new tasks in project
    while selling the property. Also worked on self module.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models,fields
+from odoo import fields,models,Command
 
 class EstateProperty(models.Model):
-    _inherit = "estate.properties"
+    _inherit =  "estate.properties"
 
     def action_sold(self):
-        print(super().action_sold())
-        # for record in self:
-        #     self.env['account.move'].create
-        #     (
-        #       {
-        #         'partner_id' : record.buyer_id.id,
-        #       'move_type' : 'out_invoice',
-        #       }  
-        #     )
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": record.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        Command.create({
+                        'name':record.name,
+                        'quantity':0.06,
+                        'price_unit':record.selling_price
+                    }),
+                    Command.create( {
+                        'name':'Administrative fees',
+                        'price_unit':100
+                    })],
+                }
+            )
         return super().action_sold()
---
+++ estate_project/models/estate_property.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields,models
+
+class EstateProperty(models.Model):
+    _inherit="estate.properties"
+
+    def action_sold(self):
+        # project= self.env["project.project"].search(["name",'=','Cleaning property']) 
+        for record in self:
+            self.env["project.task"].create({
+                'name': "clean "+ record.name,
+                'project_id' : 4,
+                "partner_id":record.buyer_id.id
+            }
+            )
+        return super().action_sold()
