93ac761e34b9155bcbc45f266ee27f17d559b00a
Om Rabara <omra@odoo.com>
2023-01-24 18:52:49 +0530

    [IMP] estate_account: invoice creation
    Create a invoice successfully.
    Done a Exercise of  6% of the selling price. an additional 100.00 from
    administrative fees.get the idea about the Command.create method.
    Create a estate_project module.
    inherit with estate property.
    Implement the bridge module with project.in that create a project and for that
    project create a multiple task when property is sold.

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models,Command
 
 
 class EstateProperty(models.Model):
@@ -7,9 +7,24 @@ class EstateProperty(models.Model):
 
     def sold_action(self):
         for record in self:
-        # print('overwrite successfully')
+            # print('overwrite successfully')
+
             self.env["account.move"].create({
                 'partner_id': record.buyer_id.id,
-                'move_type':  'out_invoice'
+                'move_type':  'out_invoice',
+                'invoice_line_ids': [
+                    Command.create({
+                    "name": record.name,
+                    "quantity": 1.0,
+                    "price_unit":record.selling_price*6.0/100,
+                },),
+                    Command.create({
+                        "name": 'administrative fees',
+                        "quantity": 1.0,
+                        "price_unit":100,
+                    })
+                ]
             })
         return super().sold_action()
+
+   
---
+++ estate_project/models/estate_property.py
@@ -0,0 +1,21 @@
+from odoo import models
+
+
+class EstatePropertypProject(models.Model):
+    _inherit = "estate.property"
+
+    def sold_action(self):
+        for record in self:
+            if not self.env['project.project'].search([('name', '=', 'Property Task')]).id:
+                project = self.env['project.project'].create({
+                'name': 'Property Task',
+                })
+            
+            
+            task_names = 'Property Maintenance.' + self.name
+            task= self.env['project.task'].create({
+                    'name': task_names,
+                    'description': 'Task for maintenance of property ' + self.name,
+                    'project_id':  self.env['project.project'].search([('name', '=', 'Property Task')]).id,
+            })
+        return super().sold_action()
