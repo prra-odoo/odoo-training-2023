539b05baa9fed5bf88d5049d8b7fd8e725227309
Om Rabara <omra@odoo.com>
2023-01-31 18:55:38 +0530

    [IMP] real_estate: complete pdf reports
    Implement the the sub-template for res.users in that fetch all the offers for
    all the properties.
    implement the report inheritance with estate account.

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -3,6 +3,10 @@
 {
     'name':'real_estate_accounting',
     'depends':['account','real_estate'],
+    'data': [
+        'report/estate_property_templates.xml',
+        
+    ],
     'application':True,
     'installable':True,
 }
