7a1269fe5da03f662ccab8c92c57c6964a38f554
Renilkumar Kajavadra <reka@odoo.com>
2023-02-02 18:59:22 +0530

    [IMP] real_estate: controller in website and page for properties in website
    Description:
    1. Implement menu for website.
    2. Implement page for properties in website.
    3. Add image to the property, and display it on front-end.

--- real_estate/models/__init__.py
+++ real_estate/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import estate_property, estate_property_controller,estate_property_type,estate_property_tag,estate_property_offer,inherited_model,estate_property_controller
+from . import estate_property,estate_property_type,estate_property_tag,estate_property_offer,inherited_model
--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -44,6 +44,7 @@ class estateProperty(models.Model):
     best_offer = fields.Float(string="Best Offer",compute="_compute_best_offer")
     status = fields.Selection(string="Status", selection=[("sold", "Sold"), ("cancel", "Cancel")])
     company_id = fields.Many2one("res.company", string="Company", required=True, default=lambda self: self.env.company)
+    image = fields.Binary(string="Image", store=True)
 
     _sql_constraints = [
         ('expected_price', 'CHECK(expected_price >= 0)', 'A property expected price should be positive.'),
