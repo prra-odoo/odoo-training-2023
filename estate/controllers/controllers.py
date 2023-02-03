307c26b2edbda4f816817e64581f14e57f62a12f
Saurabh Choraria <sauc@odoo.com>
2023-02-03 18:52:37 +0530

    [IMP] estate: Improved design of properties page and added filter
    Improved design of /estate/estate page using bootstrap classes and added filter
    search and date_availabilty

--- estate/controllers/controllers.py
+++ estate/controllers/controllers.py
@@ -14,17 +14,24 @@ class Estate(http.Controller):
         return http.request.render('estate.listing', {'plot': property})
 
     @http.route(['/estate/estate', '/estate/estate/page/<int:page>'],type="http" ,auth='public', website=True)
-    def index(self, page=1, **kw):
+    def index(self, page=1, search='', **kw):
         domain = [('state', 'in', ['new', 'offer_received'])]
+        if search:
+            domain = [('name','ilike' , search)]
         Properties = http.request.env['estate.property'].search(domain)
         total = Properties.search_count([])
+
+        date_availability = kw.get('date_availability')
+        if date_availability:
+            domain.append(('date_availability','>=', date_availability))
+
         pager = http.request.website.pager(
             url='/estate/estate',
             total=total,
             page=page,
             step=3
         )
-        pagerProperties = Properties.search(domain,offset=(page - 1) * 9, limit=10)
+        pagerProperties = Properties.search(domain,offset=(page - 1) * 4, limit=4)
         return http.request.render('estate.index', {
             'properties': pagerProperties,
             'pager':pager
