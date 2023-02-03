e260f177cd08b2ba24c5faeb1645d23229d26ad4
Saurabh Mishra <sami@odoo.com>
2023-02-03 18:57:06 +0530

    [ADD] real_estate:  Added Pagination
    Added pagination to the code.
    Made ui of website more interactive.
    Solved some errors related to

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -25,15 +25,7 @@ class Inherited_estate_property(models.Model):
                         'quantity': 1,
                     }),
                 ],
-                # 'invoice_line_ids': [(0, 0, {
-                    # 'name': record.name,
-                    # 'price_unit': (0.06*(record.selling_price)),
-                    # 'quantity': 1,
-                # }), (0,0, {
-                    # 'name': 'Administration fees',
-                    # 'price_unit': 100,
-                    # 'quantity': 1,
-                # })],
+         
             })
         return super().perform_sold()
 
--- real_estate/controllers/real_estate_web.py
+++ real_estate/controllers/real_estate_web.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.addons.portal.controllers.portal import pager as portal_pager
 
 class Estate_web(http.Controller):
     
-    @http.route('/myproperty',auth='public',website=True)
-    def mainindex(self, **kw):
-        properties = http.request.env['estate.property.model']
-        return http.request.render('real_estate.mainp',{
-            'properties': properties.search([])
+    @http.route(['/estate/home','/estate/home/page/<int:page>'],type="http" ,auth='public', website=True)
+    
+    def pagin(self, page=1,item_per_page=8,**kw):
+        
+        domain = [('state', 'in', ['new', 'offer_received'])]
+        Properties = http.request.env['estate.property.model'].search(domain)
+        total = Properties.search_count(domain)
+        
+        pager = portal_pager(
+            url='/estate/home',
+            total=total,
+            page=page,
+            step=item_per_page
+        )
+        response_property = Properties.search(domain,limit=item_per_page, offset=pager['offset'])
+
+        return http.request.render('real_estate.mainp', {
+            'properties': response_property.sudo(),
+            'pager':pager
         })
 
-    @http.route('/myproperty/properties',auth='public',website=True)
-    def index(self, **kw):
-        properties = http.request.env['estate.property.model']
-        return http.request.render('real_estate.desp',{
-            'properties': properties.search([])
-        })
 
-
-    @http.route('/myproperty/<model("estate.property.model"):id>/',auth='public',website=True)
-    def propert(self,id):
+    @http.route('/estate/home/<model("estate.property.model"):id>',auth='public',website=True)
+    def index(self, id):
         return http.request.render('real_estate.desp',{
             'property': id
         })
-
-    @http.route('/myproperty/1',auth='public',website=True)
-    def paging(self, **kw):
-        return http.request.render('real_estate.pagin')
+    
+  
