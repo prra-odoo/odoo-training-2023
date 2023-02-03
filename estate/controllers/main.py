80f9c02cb8d5fd53b383dbd27ec42625e3d0c016
Vivek Sinha <vksi@odoo.com>
2023-02-03 18:23:24 +0530

    [IMP]estate_controller : Improved the web controller
    Added search bar in the website panel.
    Added pagination and improved the module.
    Did some changes in controller.

--- estate/controllers/main.py
+++ estate/controllers/main.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from odoo import http,api
 
 class real_estate(http.Controller):
-    @http.route('/estate/estate/',auth='public' ,website=True)
-    def index(self,**kw):
-        Property =  http.request.env['estate.property']
+    @http.route(['/estate/estate/','/estate/estate/page/<int:page>'],auth='public' ,website=True)
+    def index(self,page=0,search='',**kw):
+        domain=[]
+        if search:
+            domain.append(('name','ilike','search'))
+        # Property =  http.request.env['estate.property']
+        estates = http.request.env['estate.property'].search(domain)
+        total_total = estates.sudo().search_count([])
+        pager = http.request.website.pager(
+            url='/estate/estate/',
+            total = total_total,
+            page=page,
+            step=3,
+            url_args=None,
+        )
+        package= http.request.env['estate.property'].search(domain,limit=4,offset=pager['offset'],order='id desc')
         return http.request.render('estate.index',{
-            'properties' : Property.search([]),
+            'properties' : package,
+            'pager' : pager,
         })
-        
-        
     # @http.route('/estate/<name>/', auth='public', website=True)
     # def property(self, name):
     #     return '<h1>{}</h1>'.format(name)
