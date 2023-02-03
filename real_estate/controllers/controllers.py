509e2b6aa2bfdcfa06b510b384b656138e8a4c14
Deependra Solanki <deso@odoo.com>
2023-02-03 18:52:36 +0530

    [IMP] real_estate: added date filter and search on website.
    Added date filter where user can view properties listed after a particular date.
    Also now users can search properties.

--- real_estate/controllers/controllers.py
+++ real_estate/controllers/controllers.py
@@ -1,33 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from odoo import http
+from odoo.addons.portal.controllers.portal import pager as portal_pager
 
 class RealEstate(http.Controller):
     @http.route(['/estate/home', '/estate/home/page/<int:page>'],type="http" ,auth='public', website=True)
     def index(self, page=1, **kw):
         domain = [('state', 'in', ['new', 'offer_received'])]
-        Properties = http.request.env['estate.property'].search(domain)
+        if kw.get('filterDate'):
+            domain = ['&',('date_availability', '>', kw.get('filterDate')), ('state', 'in', ['new', 'offer_received'])]
+        if kw.get('searchName'):
+            domain = ['&',('name', 'ilike', kw.get('searchName')), ('state', 'in', ['new', 'offer_received'])]
+
+        Properties = http.request.env['estate.property'].search(domain,offset=(page - 1) * 9, limit=9)
         total = Properties.search_count([])
-        pager = http.request.website.pager(
+        pager = portal_pager(
             url='/estate/home',
             total=total,
             page=page,
+            url_args={'searchName': kw.get('searchName'),'filterDate':kw.get('filterDate')},
             step=9
         )
-        pagerProperties = Properties.search(domain,offset=(page - 1) * 9, limit=9)
         return http.request.render('real_estate.index', {
-            'properties': pagerProperties,
-            'pager':pager
+            'properties': Properties,
+            'pager':pager,
+            'page':page
         })
-        
-
-    # @http.route('/estate/<name>', auth='public', website=True)
-    # def property(self, name):
-    #     return '<h1>{}</h1>'.format(name)
-
-    # @http.route('/estate/<int:id>', auth='public', website=True)
-    # def property(self, id):
-    #     return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
 
     @http.route('/estate/<model("estate.property"):name>', auth='public', website=True)
     def property(self, name):
