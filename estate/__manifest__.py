ccb2c7e2c3591ec30d19d8225cc62712efcbe288
kasp-odoo <kasp@odoo.com>
2023-02-06 17:52:14 +0530

    First Commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml'
+    ],
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
