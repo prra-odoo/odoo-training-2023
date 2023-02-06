d8ba00c253ecc8b20572af4215d30e44f7257827
Naman Shah <nash@odoo.com>
2023-02-06 18:47:36 +0530

    06-02-2023

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+	'name': "Real Estate",
+	'depends': ['base',],
+	'data':[
+		'security/ir.model.access.csv',
+		'views/estate_property_views.xml',
+		'views/estate_menus.xml'
+		],
+	
+	'application': True,
+	'license': 'LGPL-3',
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,34 @@
+from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "estate_property Model"
+    
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=datetime.now()+relativedelta(month=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=3)
+    living_area= fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string = 'Garden Orientattion',
+        selection = [('north','North'),('south','South'),
+                     ('east','East'),('west','West')],
+    )
+
+    state = fields.Selection(
+        selection = [('new','New'),('offer_received','Offer_Received'),
+                     ('offer_accepted','Offer_Accepted'),('sold','Sold'),
+                     ('cancelled','Cancelled')],
+        string = 'Status',required=True,copy=False,default='new'
+    ) 
+
+    active = fields.Boolean('Active',default=False)
