25c6bfe657de842e58eecf6f25c84304cb0c1aa2
Khushi Vakil <khva@odoo.com>
2023-02-06 18:59:14 +0530

    Third commit 06-02-2023

---
+++ estate/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Author Name",
+    'category': 'Category',
+    'license': 'LGPL-3',
+    
+    'description': """
+    Description text
+    """,
+        
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        "views/estate_menus.xml",
+    ],
+
+    'application': True,
+   'installable': True,
+
+}
