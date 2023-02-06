27ad16662fe1c3a2a5cb58c15c3a408941927157
Gitashri Mantha(gman) <gman@odoo.com>
2023-02-06 18:14:01 +0530

    first commit

---
+++ custom/estate/__manifest__.py
@@ -0,0 +1,16 @@
+# -- coding: utf-8 -
+{
+    'name': "Real Estate",
+    'depends': ['base'],
+    'application': True,
+    'data':[
+        'security/ir.model.access.csv',
+        
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+        
+    ],
+    'license':'LGPL-3'
+
+    
+}
---
+++ custom/estate/models/real_property.py
@@ -0,0 +1,35 @@
+from odoo import models,fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+
+
+class realProperty(models.Model):
+    _name = "estate.real.property"
+    _description = "Real estate model"
+    name=fields.Char(default="unknown")
+    description=fields.Char()
+    postcode=fields.Char()
+    date_availability=fields.Datetime(default=datetime.now() + relativedelta(months=3))
+    expected_price=fields.Float(required=True)
+    selling_price=fields.Float()
+    bedrooms=fields.Integer(default="2")
+    living_area=fields.Integer()
+    facades=fields.Integer()
+    garage=fields.Boolean()
+    garden=fields.Boolean()
+    garden_area=fields.Integer()
+    garden_orientation=fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'),('east', 'East'),('west', 'West')],
+        help="Select an appropriate direction")
+    active=fields.Boolean()
+    state=fields.Selection(string='State',
+        selection=[('new','New'),('recieved','Offer Recieved'),('accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')],
+        help="select the state")
+
+    
+
+
+    
