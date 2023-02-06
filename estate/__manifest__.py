de38dc5607e184a8f8d6f5132c425b716ad7dd80
Khushi Patel <khpa@odoo.com>
2023-02-06 18:56:01 +0530

    Third Commit Feb 6

---
+++ estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':"Real Estate",
+    'depends': ['base'],
+    'application': True,
+    'license' : 'LGPL-3',
+    'data' : ['views/estate_property_views.xml',
+    'views/estate_menus.xml',
+    'security/ir.model.access.csv',]
+}
