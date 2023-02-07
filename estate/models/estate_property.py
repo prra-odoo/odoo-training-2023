d79fc2a706a1297cefec484568cc32f838c16325
Jay Savaliya <jasa@odoo.com>
2023-02-07 18:42:40 +0530

    07-02-2023

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,25 +1,38 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "estate_property Model"
 
     name = fields.Char()
-    description = fields.Text()
+    description = fields.Char()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(
+        default=lambda self: fields.Datetime.now() + relativedelta(months=3),copy=False)
     expected_price = fields.Float()
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area= fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     log_access = True
     garden_orientation = fields.Selection(
-        string = 'Garden Orientattion',
-        selection = [('north','North'),('south','South'),
-                     ('east','East'),('west','West')],
+        string='Garden Orientattion',
+        selection=[('north', 'North'), ('south', 'South'),
+                   ('east', 'East'), ('west', 'West')],
     )
-    
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="Status",
+        selection=[('new', 'New'), ('offer', 'Offer'), ('recevied', 'Recevied'), ('offer_accepted', 'Offer Accepted'),
+                   ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        required=True,
+        copy=False,
+        default='new'
+    )
