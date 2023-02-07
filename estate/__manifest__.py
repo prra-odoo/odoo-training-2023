7cdf383bedf7c0061974ad2d07a3b4aff5cde07a
Akshat Trivedi <aktr.odoo.com>
2023-02-07 18:46:41 +0530

    ch-6 completed

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,5 +6,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-    ]
+        'views/estate_menus.xml',
+    ],
+    'license' : 'LGPL-3',
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,19 @@
 from odoo import models,fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
-class estate_property(models.Model):
+class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "It's a estate prooperty module"
 
 
-    name = fields.Char(required=True)
-    description = fields.Text()
+    name = fields.Char(required=True,string='Title')
+    description = fields.Char()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default = date.today() + relativedelta(months=+3),copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -22,3 +24,13 @@ class estate_property(models.Model):
         selection=[('lead','Lead'),('opportunity','Opportunity')],
         help="Type is used to separate Leads and Opportunities"
         )
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new','New'),
+        ('offer received','Offer Received'),
+        ('offer accepted','Offer Accepted'),
+        ('sold','Sold'),
+        ('canceled','Canceled')
+    ],
+    default='new',
+    )
