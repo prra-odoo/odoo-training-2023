53e9146540af4d48d879ead20bc412224d7c6c88
Adnan Saiyed <adsa@odoo.com>
2023-02-07 10:29:09 +0530

    06-02-2023

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name":"Estate",
+    "depends" : [],
+    "installable" : True,
+    "application" : True,
+    "data" : [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
+    ]
+}
---
+++ estate/models/estate_properties.py
@@ -0,0 +1,19 @@
+from odoo import models,fields
+
+class RealEstateProperties(models.Model):
+    _name = "estate.property"
+    _description = "Estate property model"
+
+    name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    
