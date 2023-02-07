84d8322765f72efda826be8f4ef0c73f6248593e
shreya patel (shpa) <shpa@odoo.com>
2023-02-07 19:00:36 +0530

    chapter 6 and list from ch-7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,45 +1,53 @@
 from odoo import models,fields
 
+from dateutil.relativedelta import relativedelta
+
 class Estate_Property(models.Model):
 	_name='estate.property'
 	_description='demo model with property trsting'
 
-	name=fields.Char(string='name',
+	name=fields.Char(string='Name',
 			required=True,
 			help="this is for name")
 
-	discription=fields.Text(string='discription',
-			help="this is for discription")
+	discription=fields.Char(string='Discription',
+			help="this is for discription",
+			size=300)
 
-	postcode=fields.Char(string='postcode',
+	postcode=fields.Char(string='Postcode',
 			help="this is for postcode")
 
-	date_availability=fields.Date(string='date availability',
-			help="this is for data availibility")
+	date_availability=fields.Date(string='Availabe From',
+			help="this is for data availibility",
+			copy=False,
+			default=lambda self:fields.Date.today()+relativedelta(months=+3))
 
-	expected_price=fields.Float(string='expected price',
+	expected_price=fields.Float(string='Expected Price',
 			required=True,
 			help="this is for expeccted price")
 
-	selling_price=fields.Float(string='selling price',
-			help="this is for selling price")
+	selling_price=fields.Float(string='Selling Price',
+			help="this is for selling price",
+			readonly=True,
+			copy=False)
 
-	bedrooms=fields.Integer(string='bedrooom',
+	bedrooms=fields.Integer(string='Bedrooom',
+			default=2,
 			help="this is for bedroom")
 
-	living_area=fields.Integer(string='living area',
+	living_area=fields.Integer(string='Living Area(sqm)',
 			help="this is for living area")
 
-	facades=fields.Integer(string='facades',
+	facades=fields.Integer(string='Facades',
 			help="this is for facades")
 
-	garage=fields.Boolean(string='garbage',
+	garage=fields.Boolean(string='Garbage',
 			help="this is for garage")
 
-	garden=fields.Boolean(string='garden',
+	garden=fields.Boolean(string='Garden',
 			help="this is for garden")
 
-	garden_area=fields.Integer(string='garden area',
+	garden_area=fields.Integer(string='Garden area',
 			help="this is for garden area")
 
 	garden_orientation=fields.Selection(selection = [('north','North'),
@@ -47,4 +55,14 @@ class Estate_Property(models.Model):
         ('east','East'),
         ('west','West')],
         string= "Garden Orientation",)
+
+
+
+	Active=fields.Boolean(default=True)
+
+	state=fields.Selection(selection=[('new','New'),('offer received','Offer Received'),
+		('offer accepted','Offer Accepted'),('sold','Sold'),('canceled','Canceled')],
+		required=True,
+		copy=False,
+		default='new')
 
