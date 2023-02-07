d7e237ecf2af0724bf0271234547490d466fbc5e
Khushi Vakil <khva@odoo.com>
2023-02-07 18:57:27 +0530

    Fourth commit 07-02-2023

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -6,14 +6,14 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "This is a real-estate property model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string= 'Title',required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default= datetime.now() + relativedelta(months=3))
+    date_availability = fields.Date(string= 'Available From',copy=False, default= datetime.now() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default="2")
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area(sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -23,3 +23,12 @@ class EstateProperty(models.Model):
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Choose appropriate direction"
     )       
+
+    active=fields.Boolean('Active', default=True)
+    state = fields.Selection(
+     string='state',
+     selection=[('new', 'New'),('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+     required=True, 
+     copy=False,
+     default='new' 
+    )
