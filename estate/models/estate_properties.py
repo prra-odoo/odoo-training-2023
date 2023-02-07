24608ca51b9e9833f42c2fe3187aa0176b146c77
Adnan Saiyed <adsa@odoo.com>
2023-02-07 19:06:26 +0530

    Upto ch6

--- estate/models/estate_properties.py
+++ estate/models/estate_properties.py
@@ -1,19 +1,23 @@
 from odoo import models,fields
+from dateutil.relativedelta import relativedelta
 
 class RealEstateProperties(models.Model):
     _name = "estate.property"
     _description = "Estate property model"
 
-    name = fields.Char()
+    name = fields.Char(default="Unknown")
+    last_seen = fields.Datetime("Last seen:",default = lambda self : fields.Datetime().now())
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default = lambda self : fields.Date.today()+relativedelta(months=3))
     expected_price = fields.Float()
     selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    
+    garden_orientation = fields.Selection(selection=[("north","North"),("south","South"),("east","East"),("west","West")])
+    active = fields.Boolean(default = False)
+    status = fields.Selection(selection=[("new","New"),("offer received","Offer Received"),("offer accepted","Offer Accepted"),("sold","Sold"),("cancelled","Cancelled")])
