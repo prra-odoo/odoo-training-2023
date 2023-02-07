de6c6e44ef4d0024c60b53e7ad5faeb4bbf43e45
Khushi Patel <khpa@odoo.com>
2023-02-07 19:07:08 +0530

    Fourth Commit Feb 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,18 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property Model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True,default="Unknown")
+    last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=lambda self: fields.Datetime.now()+relativedelta(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,3 +22,9 @@ class EstateProperty(models.Model):
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Type is used to separate Leads and Opportunities")
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='State',
+        selection=[('new', 'New'), ('offer received', 'Offer Received'), 
+        ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help="What's the Status!",default="new",required=True,copy=False)
