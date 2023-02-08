22ebfa90adf9d1820b400d0ce168c2ea7516d67f
RajPatani <rapa@odoo.com>
2023-02-08 18:56:17 +0530

    [ADD] estate:  Added Estate Project
    Executed Addition of the Estate project which will act as a bridge module to
    create tasks related to the properties sold via Real Estate.

---
+++ estate_project/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name':"Estate Property Project",
+    'version': "1.0",
+    'installable':True,
+    'depends':['project','estate'],
+    'author': "Raj Patani (rapa)"
+}
---
+++ estate_project/models/estate_property_project.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import fields,models
+
+class EstatePropertyProject(models.model):
+    _inherit = "estate.property"
+
+    
