2c666f85c66e777bbade50a50f00a29af55b20a8
Ayushi Gorai <aygo@odoo.com>
2023-02-08 19:02:10 +0530

    [IMP] estate:import the bridge module with account and project

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Real Estate Accounting",
+    'author': "Ayushi Gorai(aygo)",
+    'summary': "Bridge Module for account and estate",
+    'category': 'Category',
+    'license': 'LGPL-3',
+    'depends': [
+        'estate',
+        'account',
+        'project',
+    ],
+    'auto_install': True,
+    'installable':True,
+    'application':True,
+}
