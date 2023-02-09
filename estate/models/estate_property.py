9bcf1b391aac4bd4c831482b51f5cf7a0d28f9dd
Vivek Pathak <vivp@odoo.com>
2023-02-09 18:48:58 +0530

    upto many2one

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -28,3 +28,7 @@ class EstateProperty(models.Model):
         selection = [('north','North'),('South','South'),('East','East'),('West','West'), ],
         help = "Choose the direction"
     )
+    property_type_id = fields.Many2one("estate.property.type",string="Property Type")
+    buyer = fields.Many2one("res.partner",string="Buyer",copy=False)
+    salesperson = fields.Many2one("res.users",string="Salesman",default=lambda self: self.env.user)
+    
