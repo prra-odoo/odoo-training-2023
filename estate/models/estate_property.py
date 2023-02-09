7f7136811aa38fb27f2de01bb33a3d47f7dac0cd
Vivek Pathak <vivp@odoo.com>
2023-02-09 10:10:05 +0530

    upto ch 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,21 +1,28 @@
 from odoo import models,fields
+from odoo.tools.rendering_tools import relativedelta_proxy
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description='Real Estate Property'
     
     name = fields.Char(required=True)
-    description = fields.Text()
+    description = fields.Char()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False,default = lambda self : fields.Date.today()+relativedelta_proxy(months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy= False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    active = fields.Boolean(default = True)
+    state = fields.Selection(
+        string="State",
+        default="new",
+        selection=[('new','New'),('offer received','Offer Received'),('offer accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')]
+    )
     garden_orientation = fields.Selection(
         string = "Type",
         selection = [('north','North'),('South','South'),('East','East'),('West','West'), ],
---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,12 @@
+from odoo import models ,fields
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = "Real Estate Property Type"
+
+    name = fields.Char(required=True)
+    
+        
+
+
+    
