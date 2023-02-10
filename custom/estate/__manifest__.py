8877472d86aba081d22dd604beff1d203867a3e6
Gitashri Mantha(gman) <gman@odoo.com>
2023-02-10 18:45:33 +0530

    fifth commit

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -7,6 +7,8 @@
         'security/ir.model.access.csv',
         
         'views/estate_property_views.xml',
+        'views/estate_property_tags.xml',
+        'views/estate_property_types.xml',
         'views/estate_menus.xml'
         
     ],
