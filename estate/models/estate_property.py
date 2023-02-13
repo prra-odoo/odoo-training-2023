4f7b26eb6ac3fc06d361bdd0bcde6432fcb278fc
Aman Patel <ampa@odoo.com>
2023-02-13 19:02:13 +0530

    estate upto chapter 9

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import models,fields,api,exceptions
 from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
@@ -40,3 +40,43 @@ class EstateProperty(models.Model):
     seller_id = fields.Many2one('res.users',string="Seller", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag",string="Property Tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+    total_area = fields.Integer()
+    
+    total_area = fields.Integer(compute="_total_area",store=True)
+    @api.depends('living_area','garden_area')
+    def _total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    best_offer = fields.Float(compute='_best_offer',store=True)
+    @api.depends("offer_ids")
+    def _best_offer(self):
+        for record in self:
+            if(record.offer_ids):
+                record.best_offer=max(record.offer_ids.mapped("price"))
+            else:
+                record.best_offer=0.0
+    # onchange function when garden field is enabled
+    @api.onchange('garden')
+    def onchange_garden(self):
+        for record in self:
+            if record.garden:
+                record.garden_area = 10.0
+                record.garden_orientation = 'north'
+            else:
+                record.garden_area = 0.0
+                record.garden_orientation = False
+            
+    #function making two button of sold and cancelled in header
+    def action_cancel(self):
+        for record in self:
+            if record.state == 'sold':
+                raise exceptions.UserError("sold property can't be cancelled.")
+            else:record.state = 'cancelled'
+    def action_sold(self):
+        for record in self:
+            if record.state == 'cancelled':
+                raise exceptions.UserError("cancelled property can't be sold.")
+            else: record.state = 'sold'
+
+        
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models,fields
+from odoo import models,fields,api
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyOffer(models.Model):
     
@@ -12,3 +13,30 @@ class EstatePropertyOffer(models.Model):
         copy=False)
     partner_id = fields.Many2one("res.partner",string="partner",required=True)
     property_id = fields.Many2one("estate.property",required=True)
+    validity = fields.Integer(default = "7")
+    create_date = fields.Date(default=fields.Datetime.today())
+
+# for setting the deadline and even dynamically changing it
+    date_deadline = fields.Date(compute="_compute_date",inverse="_inverse_date",store=True)
+    @api.depends('create_date','validity')
+    def _compute_date(self):
+        for record in self:
+            record.date_deadline = record.create_date + relativedelta(days=record.validity)
+
+    def _inverse_date(self):
+        for record in self:
+            if record.create_date:
+                record.validity  = (record.date_deadline - record.create_date).days
+
+
+            
+# now from here the ACCEPT and REFUSE state code starts
+
+    def accept_offer(self):
+        self.status = "accepted"
+        
+        
+    
+    def refuse_offer(self):
+        self.status = "refused"
+    
