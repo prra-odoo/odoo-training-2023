1bd5b91fe6211842339509f5b99053d3af856451
kasp-odoo <kasp@odoo.com>
2023-02-14 18:58:12 +0530

    CH-9 (Up to Inverse Function)

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
 
 
@@ -23,15 +23,31 @@ class EstateProperty(models.Model):
         selection = [('north', 'North'), ('east', 'East'),
                    ('south', 'South'), ('west', 'West')]
     )
-    active = fields.Boolean('Active', default=True)
+    active = fields.Boolean(default=True)
     state = fields.Selection(
         selection = [('new','New'),
                      ('offer_received','Offer Received'),
                      ('offer_accepted','Offer Accepted'),
                      ('sold','Sold'),
-                     ('canceled','Canceled')], string='State', default='new', copy=False)
+                     ('canceled','Canceled')], default='new', copy=False)
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     user_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
-    tag_ids = fields.Many2many('estate.property.tag', string='Property Tag')
+    tag_ids = fields.Many2many('estate.property.tag', relation='estate_property_tag_rel', string='Property Tag')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offer')
+    total_area = fields.Integer(compute='_compute_total', string='Total Area')
+    best_price = fields.Float(compute='_compute_best_offer', string='Best Offer')
+
+    @api.depends('living_area','garden_area')
+    def _compute_total(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids')
+    def _compute_best_offer(self):
+        for record in self:
+                if(record.offer_ids):
+                     record.best_price = max(record.offer_ids.mapped("price"))
+                else:
+                     record.best_price = 0.0
+                     
