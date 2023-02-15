791e4dac01e684f21c0e1837673f55ce5cfec1a4
priyanshi patel(prpa) <prpa@odoo.com>
2023-02-15 18:52:07 +0530

    real estate complete ch-9 and ch-10(1st part)

--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -1,6 +1,7 @@
 from odoo import fields,models,api
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -17,11 +18,12 @@ class EstateProperty(models.Model):
     total_area=fields.Float(compute="_total_area",readonly=True)
     facades = fields.Integer()
     garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden = fields.Boolean(store=True)
+    
+    garden_area = fields.Integer(compute="_compute_area",store=True,readonly=False)
     best_price = fields.Float(compute="_compute_discount")
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Type',compute="_compute_area",readonly=False,
         selection = [('north','North'),('south','South'),('east','East'),('west','West')],
         help="Type is used to separate Directions"
     )
@@ -33,12 +35,12 @@ class EstateProperty(models.Model):
     )
 
     property_type_id=fields.Many2one('estate.property.type',string="Property Type")
-    property_tag_id=fields.Many2many('estate.property.tag',relation='property_tag_rel',string="Property Tag")
-    offer_id=fields.One2many('estate.property.offer','property_id',string="Offer")
+    property_tag_ids=fields.Many2many('estate.property.tag',relation='property_tag_rel',string="Property Tag")
+    offer_ids=fields.One2many('estate.property.offer','property_id',string="Offer")
 
 
-    buyers=fields.Many2one('res.partner',copy=False)
-    salesmen=fields.Many2one('res.users',default=lambda self:self.env.user)
+    buyers_id=fields.Many2one('res.partner',copy=False)
+    salesmen_id=fields.Many2one('res.users',default=lambda self:self.env.user)
 
     @api.depends("living_area","garden_area")
     def _total_area(self):
@@ -46,10 +48,32 @@ class EstateProperty(models.Model):
             record.total_area = record.living_area + record.garden_area
 
 
-    @api.depends("offer_id")
+    @api.depends("offer_ids")
     def _compute_discount(self):
         for record in self:
-            if(record.offer_id): 
-                record.best_price = max(record.offer_id.mapped("price"))
+            if(record.offer_ids): 
+                record.best_price = max(record.offer_ids.mapped("price"))
             else:
                 record.best_price=0.0 
+
+    @api.depends("garden")
+    def _compute_area(self):
+        for record in self:
+            if record.garden:
+                record.garden_area=10
+                record.garden_orientation='north'
+            else:
+                record.garden_area=False
+                record.garden_orientation=False
+
+    def action_sold(self):
+        for record in self:
+            if self.mapped("state"):
+                raise UserError("Cancelled properties not sold.")
+            return self.write({"state": "sold"})
+            
+    def action_cancle(self):
+        for record in self:
+            if self.mapped("state"):
+                 raise UserError("Sold properties not be cancelled.")
+            return self.write({"state": "canceled"})
--- real_estate/models/estate_property_offer.py
+++ real_estate/models/estate_property_offer.py
@@ -13,24 +13,32 @@ class EstatePropertyOffer(models.Model):
         copy=False
      )
     validity=fields.Integer(default=7)
-    date_deadline=fields.Date(compute="_compute_deadline")
+    date_deadline=fields.Date(compute="_compute_date",inverse="_inverse_date")
     partner_id = fields.Many2one('res.partner',required=True)
     property_id = fields.Many2one('estate.property',required=True)
 
-    # @api.depends('validity')    
+    @api.depends('create_date','validity')
     # def _compute_date(self):
     #     for record in self:
-    #         record.date_deadline = record.create_date.date() + relativedelta(days=+record.validity)
+    #         if(not record.create_date):
+    #             record.create_date=fields.date.today()
+    #         record.date_deadline = record.create_date.date()+relativedelta(days=+record.validity)
 
-    # def _inverse_date(self):
-    #     for record in self:
-    #         record.validity=(record.date_deadline-record.create_date).days()
-    @api.depends('validity')
-    def _compute_deadline(self):
+    #         @api.depends("create_date", "validity")
+    def _compute_date(self):
         for record in self:
-            record.date_deadline = record.create_date.date()+relativedelta(days=+record.validity)
-    def _inverse_deadline(self):
+            if record.create_date:
+                date = record.create_date.date()
+                record.date_deadline = date + relativedelta(days=record.validity)
+            else:
+                date=datetime.today()
+                record.date_deadline = date + relativedelta(days=record.validity)
+
+    def _inverse_date(self):
         for record in self:
-            record.validity = (record.date_deadline - record.create_date.date()).days
+            if(record.date_deadline > datetime.date(datetime.today())):
+                record.validity = (record.date_deadline - record.create_date.date()).days
+            else:
+                record.validity=0
 
-
+   
