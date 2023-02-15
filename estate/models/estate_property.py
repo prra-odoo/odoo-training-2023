11dc5da930f759f986576756d96e69fd3231a418
Vivek Pathak <vivp@odoo.com>
2023-02-15 19:00:25 +0530

    upto ch-9

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -16,34 +16,56 @@ class EstateProperty(models.Model):
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(compute="_compute_garden",readonly = False, store = True)
     active = fields.Boolean(default = True)
     state = fields.Selection(
         string="State",
         default="new",
         selection=[('new','New'),('offer received','Offer Received'),('offer accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')]
     )
+    
     garden_orientation = fields.Selection(
         string = "Type",
         selection = [('north','North'),('South','South'),('East','East'),('West','West'), ],
-        help = "Choose the direction"
+        help = "Choose the direction",
+        compute = "_compute_garden",
+        readonly = False,
+        store = True
     )
+    
     property_type_id = fields.Many2one("estate.property.type",string="Property Type")
     buyer_id = fields.Many2one("res.partner",string="Buyer",copy=False)
     salesperson_id = fields.Many2one("res.users",string="Salesman",default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tags", string="Property Tags")
     offer_ids = fields.One2many("estate.property.offer","property_id", string="Offers")
     total_area = fields.Integer(compute="_compute_total")
-    best_offer = fields.Float(compute = "_best_offer")
+    best_offer = fields.Float(compute = "_compute_bestoffer")
     
-    @api.depends("living_area","garden_area")
+    #compute for total_area
+    
+    @api.depends("living_area","garden_area") #decorator
     
     def _compute_total(self):
-        for record in self:
+        for record in self: 
             record.total_area = record.living_area + record.garden_area
+    
+    #compute for best_offer
             
     @api.depends("offer_ids.price")
     
-    def _best_offer(self):
+    def _compute_bestoffer(self):
         for record in self:
-            record.best_offer = max(record.offer_ids.mapped("price")) 
+            if record.offer_ids.mapped("price"):
+                record.best_offer = max(record.offer_ids.mapped("price"))
+            else:
+                record.best_offer = 0
+    
+    #compute for garden_orientation and garden_area
+    
+    @api.depends("garden")
+    
+    def _compute_garden(self):
+        for record in self:
+                record.garden_orientation = "north" if record.garden == True else ""
+                record.garden_area = 10 if record.garden == True else ""
+                
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,7 @@
+from datetime import date
+from isodate import strftime
 from odoo import models,fields,api
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyOffer(models.Model):
     _name="estate.property.offer"
@@ -9,19 +12,30 @@ class EstatePropertyOffer(models.Model):
         copy=False,
         selection=[('accepted','Accepted'),('refused','Refused')]
     )
+    
     partner_id=fields.Many2one("res.partner",string="Partner", required=True)
     property_id=fields.Many2one("estate.property",string="Property Id", required=True)
     validity=fields.Integer(default=7)
-    date_deadline=fields.Date(compute="_compute_deadline",inverse = "_inverse_deadline")
+    date_deadline=fields.Date(compute="_compute_date_deadline",inverse = "_inverse_date_deadline")
+    
+    #compute for date_deadline
     
     @api.depends("create_date","validity")
     
-    def _compute_deadline(self):
+    def _compute_date_deadline(self):
         for record in self:
-            record.date_deadline = record.create_date + record.validity
-    
-    def _inverse_deadline(self):
+            if record.create_date:
+                record.date_deadline = record.create_date + relativedelta(days=record.validity)
+            else :
+                record.date_deadline = False 
+                
+    #inverse of date_deadline
+         
+    def _inverse_date_deadline(self):
         for record in self:
-            record.validity = record.date_deadline-record.create_date
+            record.validity = int(record.date_deadline.strftime("%d")) - int(record.create_date.strftime("%d"))
             
-    
+        #strftime() used to convert datetime object into string
+        #strftime("%d") to extract date
+        #strftime("%m") for month 
+        #strftime("%Y") for year
