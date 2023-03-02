ed58624c1d7328743f383a113a8b8ebaca5a0245
Adnan Saiyed <adsa@odoo.com>
2023-03-02 18:59:29 +0530

    Upto ch 15

--- estate_project/models/estate_property.py
+++ estate_project/models/estate_property.py
@@ -1,12 +1,26 @@
-from odoo import models,fields
+from odoo import models,fields,Command
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_sold(self):
-        project = self.env['project.project'].create({
+    
+        project_obj = self.env['project.project'].create({
             'name': 'Estate Project',
-            'allow_timesheets': True,
             'partner_id': self.buyer_id.id,
+            'type_ids': [
+                Command.create({'name': 'Renovation', 'sequence': 1, 'fold': False}),
+                Command.create({'name': 'Paint', 'sequence': 2, 'fold': False}),
+                Command.create({'name': 'Payment and Paper Work', 'sequence': 3, 'fold': False})
+            ]
         })
+        task_obj_1 = self.env['project.task'].create({
+            'name': 'Task 1',
+            'project_id': project_obj.id
+        })
+        task_obj_2 = self.env['project.task'].create({
+            'name': 'Task 2',
+            'project_id': project_obj.id
+        })
+        
         return super().action_sold()
