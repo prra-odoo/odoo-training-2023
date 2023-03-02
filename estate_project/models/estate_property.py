9a96d0c36c8fb6b5191d50af334b578fdc7c8b8d
Khushi Patel <khpa@odoo.com>
2023-03-02 18:59:58 +0530

    [ADD] estate: Created kanban view for properties

--- estate_project/models/estate_property.py
+++ estate_project/models/estate_property.py
@@ -6,6 +6,6 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def action_sold(self):
-        values = {'name':self.name,'partner_id':self.buyer_id.id}
+        values = {'name':self.name,'partner_id':self.buyer_id.id,'task_ids':[Command.create({"name":"Maintainance"})]}
         self.env['project.project'].create(values)    
         return super().action_sold()
