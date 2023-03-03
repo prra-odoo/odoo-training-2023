2e071cb5af2c6da7e039667565c83a03f8a71edc
Astik Singh <assi@odoo.com>
2023-03-03 18:56:12 +0530

    Learn Qweb and build report generate pdf

--- Custom/Estate/__manifest__.py
+++ Custom/Estate/__manifest__.py
@@ -6,16 +6,19 @@
     'license':'LGPL-3',
     'application':True,
     'installable':True,
+    'demo':[
+        'demo/real_estate_demo_data.xml',
+    ],
     'data':[
         'security/ir.model.access.csv',
         'views/estate_property_offer.xml',
-        'views/estate_property_views.xml',
         'views/estate_property_tag.xml',
         'views/estate_Type.xml',
+        'views/estate_property_views.xml',
+        'report/estate_report.xml',
+        'report/estate_template.xml',
+        'report/res_user_template.xml',
         'views/estate_menus.xml',
-    ],
-    'demo':[
-        'demo/real_estate_demo_data.xml',
     ]
 }
 
