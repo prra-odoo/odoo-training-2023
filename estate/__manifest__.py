4d30c1d942bd5e3339792976f48a78f8eb143802
Naman Shah <nash@odoo.com>
2023-03-03 19:01:56 +0530

    [IMP] estate : build PDF reports
    create PDF reports with templates and also use sub templates for create reports.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,10 +4,12 @@
 	'depends': ['base','mail'],
 	'data':[
 		'security/ir.model.access.csv',
-		'views/estate_property_views.xml',
 		'views/estate_property_offers_views.xml',
-		'views/estate_property_type_views.xml',
 		'views/estate_property_tags_views.xml',
+		'views/estate_property_type_views.xml',
+		'report/estate_property_reports.xml',
+		'report/estate_property_templates.xml',
+		'views/estate_property_views.xml',
 		'views/estate_menus.xml'
 		],
 	'image':['static/description/icon.png'],
