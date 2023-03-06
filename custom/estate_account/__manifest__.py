466d045506ab9742006a5bddb6773d717f0f344a
Gitashri Mantha(gman) <gman@odoo.com>
2023-03-06 19:04:10 +0530

    [ADD] estate created report inheritance feature, still some bugs left

--- custom/estate_account/__manifest__.py
+++ custom/estate_account/__manifest__.py
@@ -3,6 +3,12 @@
     'name': "Real Estate Account",
     'depends': ['estate','account'],
     'application':True,
+    'data':[
+    'report/estate_account_reports.xml',
+    'report/estate_account_templates.xml'
+    
+    
+    ]
     
 
     
