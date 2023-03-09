8b8747c846a5ece18c19cfb01c0074ac009fd7e7
Aman Patel <ampa@odoo.com>
2023-03-09 18:59:04 +0530

    [IMP] real_estate:Grid view and designing of template

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,11 @@
 #--coding: utf-8 -
 {
     'name': "Aman Real Estate",
-    'depends': ['base', 'mail',  'portal'],
+    'depends': ['base', 'mail',  'portal','website'],
     'application': True,
     'license' : 'LGPL-3',
     'data':['security/ir.model.access.csv',
+            'security/security.xml',
             'report/estate_property_reports.xml',
             'report/estate_property_templates.xml',
             'views/estate_property_offer_views.xml',
@@ -12,9 +13,9 @@
             'views/estate_property_type_views.xml',
             'views/estate_property_tag_views.xml',
             'views/res_users_views.xml',
-            'views/estate_menus.xml',
             'data/estate_property_master_data.xml',
-            'security/security.xml'
+            'controller/templates.xml',
+            'views/estate_menus.xml'
             ],
     'demo' : ['demo/realestate_demo_data.xml'],
     'category': 'Real Estate/Brokerage'
--- estate/controller/controllers.py
+++ estate/controller/controllers.py
@@ -1,9 +1,31 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 from odoo import http
 
 class Controllers(http.Controller):
 
-    @http.route('/academy/academy/', auth='public',website = True)
-    def index(self):
-        return "Hello, world"
+    @http.route(['/properties/','/properties/page/<int:page>'], auth='public', website=True)
+    def index(self,page=0,search='', **kw):
+        domain=[]
+        if search:
+            domain.append(('name','ilike',search))
+        Properties = http.request.env['estate.property'].search(domain)
+        total_properties = Properties.sudo().search_count([])
+        pager = http.request.website.pager(
+            url='/properties/',
+            total = total_properties,
+            page=page,
+            step=4,
+            url_args=None,
+        )
+        package = http.request.env['estate.property'].search(domain,limit=4,offset=pager['offset'],order='id desc')
+        return http.request.render('estate.properties_template',{
+            'properties' : package,
+            'pager' : pager,
+        })
+
+    @http.route('/properties/<model("estate.property"):properties>/',auth='public',website=True)
+    def property(self,properties):
+        return http.request.render('estate.template_controller',{
+            'prop' : properties
+        })
     
